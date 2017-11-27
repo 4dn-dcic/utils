d5e407058d8eb3fb7f17107e7d12d4b0058c6659
j1z0 <jeremy@softworks.com.my>
2017-11-27 14:03:29 -0500

    getting this put together

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+# variables used in buildout
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+requires = [
+    'Submit4DN',
+    'boto3',
+]
+
+tests_require = [
+    'pytest',
+    'pytest-mock',
+    'pytest-cov',
+]
+
+setup(
+    name='DCIC Utils',
+    version='0.1',
+    description='Utility modules shared amongst several repos in the 4dn-dcic organization',
+    long_description=README,
+    packages=['dcicutils'],
+    include_package_data=True,
+    zip_safe=False,
+    author='$dn Team at Harvard Medical School',
+    author_email='jeremy_johnson@hms.harvard.edu',
+    url='https://data.4dnucleome.org',
+    license='MIT',
+    install_requires=requires,
+    tests_require=tests_require,
+    extras_require={
+        'test': tests_require,
+    },
+)
