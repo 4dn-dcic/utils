783be338e6a1802643159e454d1670bb3e36c3ce
Carl Vitzthum <carl.vitzthum@gmail.com>
2018-11-28 16:36:17 -0500

    Extracted find_valid_file_or_extra_file function and made locate_4dn_file and mount_4dn_file functions

--- dcicutils/jh_utils.py
+++ dcicutils/jh_utils.py
@@ -10,7 +10,6 @@ if sys.version_info[0] == 3:
 else:
     import urllib2 as use_urllib
 
-
 # do some top level stuff when the module is imported
 if 'FF_ACCESS_KEY' not in os.environ or 'FF_ACCESS_SECRET' not in os.environ:
     raise Exception('ERROR USING JUPYTERHUB_UTILS! FF_ACCESS_KEY and/or '
@@ -107,6 +106,83 @@ def automatically_decorate_handled_functions():
             setattr(this_module, fxn_name, jupyterhub_authenticator_deco(fxn_obj))
 
 
+def find_valid_file_or_extra_file(obj_id, format):
+    """
+    Helper function. Get the file metadata for the given obj_id and display
+    other file formats that could also be downloaded, if extra_files is present
+    on the object.
+    Returns a dict with the following keys: metadata, full_href, full_path
+    """
+    try:
+        file_meta = get_metadata(obj_id)  # NOQA
+    except Exception as exc:
+        raise Exception('Could not open file: %s. Reason: %s' % (obj_id, exc))
+
+    if ('upload_key' not in file_meta or 'href' not in file_meta or 'File' not in file_meta.get('@type', [])):
+        raise Exception('Could not open file: %s. Reason: it is not a valid file object.' % obj_id)
+
+    main_ff = file_meta['file_format']['display_title']
+    all_ffs = {main_ff: {'uk': file_meta['upload_key'], 'href': file_meta['href']}}
+    ff_display = main_ff + ' (default)\n'
+
+    if len(file_meta.get('extra_files', [])) > 0:
+        for extra_file in file_meta['extra_files']:
+            if 'file_format' not in extra_file or 'upload_key' not in extra_file:
+                continue
+            extra_ff = extra_file['file_format']['display_title']
+            all_ffs[extra_ff] = {'uk': extra_file['upload_key'], 'href': extra_file['href']}
+            ff_display += (extra_ff + '\n')
+        if format is None and len(all_ffs) > 1:  # notify the users of extra files
+            print('There are extra files associated with the given file ID (%s). '
+                  'The file formats of those files are:\n%sTo open an extra '
+                  'file, add the following parameter: `format=<chosen format>`'
+                  % (obj_id, ff_display))
+
+    if format and format not in all_ffs:
+        raise Exception('Could not open file: %s. Reason: invalid file format '
+                        'supplied. Use one of the following:\n%s' % (obj_id, ff_display))
+    use_ff = format if (format is not None) else main_ff
+    # the full fourfront href for the download
+    full_href = '/'.join([DATA_SERVER, all_ffs[use_ff]['href']])
+
+    # get the full Jupyterhub path for the file
+    home_dir = '/home/jovyan'
+    # hardcoded so that we can find which volume to look in by file type
+    # will need to be updated if more File types are added to proc file bucket
+    if 'FileVistrack' in file_meta['@type'] or 'FileProcessed' in file_meta['@type']:
+        data_dir = 'proc_data'
+    else:
+        data_dir = 'raw_data'
+    full_path = '/'.join([home_dir, data_dir, all_ffs[use_ff]['uk']])
+    return {'metadata': file_meta, 'full_href': full_href, 'full_path': full_path}
+
+
+def locate_4dn_file(obj_id, format=None):
+    """
+    Use this function to return the full download href for a file on the
+    Fourfront data portal. Handles finding of extra_files using the `format`
+    parameter
+    """
+    file_info = find_valid_file_or_extra_file(obj_id, format)
+    return file_info['full_href']
+
+
+def mount_4dn_file(obj_id, format=None):
+    """
+    Use this function to return the full filepath to a 4dn file on the
+    Jupyterhub filesystem.
+    EVENTUALLY this function will actually mount the file, but for now it will
+    just direct the user to the pre-mounted file. Actually do a FF metadata
+    check as well, and handle finding of extra_files using the `format`
+    parameter
+    """
+    file_info = find_valid_file_or_extra_file(obj_id, format)
+    # for now, ensure the file exists. Will catch non-uploaded files
+    if not os.path.isfile(file_info['path']):
+        raise Exception('Could not open file: %s. Reason: file cannot be mounted.' % obj_id)
+    return file_info['full_path']
+
+
 @contextmanager
 def open_4dn_file(obj_id, format=None, local=True):
     """
@@ -128,50 +204,21 @@ def open_4dn_file(obj_id, format=None, local=True):
     local JH volume where goofys is mounted. Setting local to False will cause
     urllib to be open. Eventually, this will be the default behavior.
     """
-    try:
-        file_meta = get_metadata(obj_id)  # NOQA
-    except Exception as exc:
-        raise Exception('Could not open file: %s. Reason: %s' % (obj_id, exc))
-
-    if ('upload_key' not in file_meta or 'href' not in file_meta or 'File' not in file_meta.get('@type', [])):
-        raise Exception('Could not open file: %s. Reason: it is not a valid file object.' % obj_id)
-
-    main_ff = file_meta['file_format']['display_title']
-    all_ffs = {main_ff: {'uk': file_meta['upload_key'], 'href': file_meta['href']}}
-
-    if len(file_meta.get('extra_files', [])) > 0:
-        ff_display = main_ff + ' (default)\n'
-        for extra_file in file_meta['extra_files']:
-            if 'file_format' not in extra_file or 'upload_key' not in extra_file:
-                continue
-            extra_ff = extra_file['file_format']['display_title']
-            all_ffs[extra_ff] = {'uk': extra_file['upload_key'], 'href': extra_file['href']}
-            ff_display += (extra_ff + '\n')
-        if format is None and len(all_ffs) > 1:  # notify the users of extra files
-            print('There are extra files associated with the given file ID (%s). '
-                  'The file formats of those files are:\n%sTo open an extra '
-                  'file, use `open_4dn_file(%s, format=<chosen format>)`'
-                  % (obj_id, ff_display, obj_id))
-
-    if format and format not in all_ffs:
-        raise Exception('Could not open file: %s. Reason: invalid file format '
-                        'supplied. Use one of the following:\n%s' % (obj_id, ff_display))
-    use_ff = format if (format is not None) else main_ff
-    file_path = all_ffs[use_ff]['uk']
-    file_href = all_ffs[use_ff]['href']
-
-    if not local:  # this will be the base case in the future...
-        full_href = '/'.join([DATA_SERVER, file_href])
-        ff_file = use_urllib.urlopen(full_href)
+    file_info = find_valid_file_or_extra_file(obj_id, format)
+    # this will be the base case in the future...
+    if not local:
+        # this seems to handle both binary and non-binary files
+        ff_file = use_urllib.urlopen(file_info['full_href'])
     else:
-        home_dir = '/home/jovyan'
-        # hardcoded so that we can find which volume to look in by file type
-        if 'FileVistrack' in file_meta['@type'] or 'FileProcessed' in file_meta['@type']:
-            data_dir = 'proc_data'
+        ff_file = open(file_info['full_path'])
+        # see if the file is binary (needs to opened with 'rb' mode)
+        # try to read a line from the file; if it is read, reset with seek()
+        try:
+            ff_file.readline()
+        except UnicodeDecodeError:
+            ff_file = open(file_info['full_path'], 'rb')
         else:
-            data_dir = 'raw_data'
-        jh_path = '/'.join([home_dir, data_dir, file_path])
-        ff_file = open(jh_path)
+            ff_file.seek(0)
     try:
         yield ff_file
     finally:
