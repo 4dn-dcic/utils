a93c116bf8ee7d337c809a7fad304ba54b50417c
Carl Vitzthum <carl.vitzthum@gmail.com>
2019-08-29 17:05:54 -0400

    Upgrade urllib and make installation deps more flexible. Also fix setup long_description

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
-import os
+import io
 from setuptools import setup
+from os import path
 
-# variables used in buildout
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
+this_directory = path.abspath(path.dirname(__file__))
+with io.open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
-# we want strict package requirements on install
-with open('requirements.txt') as f:
-    requires = f.read().splitlines()
-requires = [req.strip() for req in requires]
+requires = [
+    'boto3>=1.7.42',
+    'botocore>=1.10.42',
+    'elasticsearch==5.5.3',
+    'aws_requests_auth>=0.4.1',
+    'urllib3>=1.23',
+    'structlog>=18.1.0',
+    'requests>=2.20.0'
+]
 
 tests_require = [
     'pytest',
@@ -21,12 +27,14 @@ setup(
     name='dcicutils',
     version=open("dcicutils/_version.py").readlines()[-1].split()[-1].strip("\"'"),
     description='Utility modules shared amongst several repos in the 4dn-dcic organization',
-    long_description=README,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=['dcicutils'],
     include_package_data=True,
     zip_safe=False,
     author='4DN Team at Harvard Medical School',
-    author_email='burak_alver@hms.harvard.edu',
+    author = "Carl Vitzthum",
+    author_email = "carl_vitzthum@hms.harvard.edu",
     url='https://data.4dnucleome.org',
     license='MIT',
     install_requires=requires,
