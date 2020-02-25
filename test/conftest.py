ceef2cd733df84434872087248756399df5c469d
SooLee <duplexa@gmail.com>
2020-02-25 19:22:48 +0000

    fixed bug in conftest

--- test/conftest.py
+++ test/conftest.py
@@ -46,12 +46,12 @@ def integrated_s3_info():
     zip_filename2 = '__test_data/madqc_report.zip'
     s3Obj = s3Utils(env=INTEGRATED_ENV)
     # for now, always upload these files
-    s3Obj.s3.put_object(Bucket=s3Obj.sys_bucket, Key=test_filename,
+    s3Obj.s3.put_object(Bucket=s3Obj.outfile_bucket, Key=test_filename,
                           Body=str.encode('thisisatest'))
     zip_path = os.path.join('test', 'data_files', zip_filename.split('/')[-1])
-    s3Obj.s3.upload_file(Filename=str(zip_path), Bucket=s3Obj.sys_bucket, Key=zip_filename)
+    s3Obj.s3.upload_file(Filename=str(zip_path), Bucket=s3Obj.outfile_bucket, Key=zip_filename)
     zip_path2 = os.path.join('test', 'data_files', zip_filename2.split('/')[-1])
-    s3Obj.s3.upload_file(Filename=str(zip_path2), Bucket=s3Obj.sys_bucket, Key=zip_filename2)
+    s3Obj.s3.upload_file(Filename=str(zip_path2), Bucket=s3Obj.outfile_bucket, Key=zip_filename2)
 
     return {'s3Obj': s3Obj, 'filename': test_filename, 'zip_filename': zip_filename,
             'zip_filename2': zip_filename2}
