963755abd9b98ac5c07722d6a22cc9639768ee3d
Kent Pitman <kent_pitman@hms.harvard.edu>
2020-04-17 10:02:14 -0400

    New functionality: Support for 'with local_attrs(obj, attr1=val1, attr2=val2):'.

--- dcicutils/qa_utils.py
+++ dcicutils/qa_utils.py
@@ -2,6 +2,7 @@
 qa_utils: Tools for use in quality assurance testing.
 """
 
+import contextlib
 from .misc_utils import PRINT
 
 
@@ -18,3 +19,35 @@ def mock_not_called(name):
         PRINT("kwargs=", kwargs)
         raise AssertionError("%s was called where not expected." % name)
     return mocked_function
+
+
+@contextlib.contextmanager
+def local_attrs(obj, **kwargs):
+    """
+    This binds the named attributes of the given object.
+    This is only allowed for an object that directly owns the indicated attributes.
+
+    """
+    keys = kwargs.keys()
+    for key in keys:
+        if key not in obj.__dict__:
+            # This works only for objects that directly have the indicated property.
+            # That happens for
+            #  (a) an instance where its instance variables are in keys.
+            #  (b) an uninstantiated class where its class variables (but not inherited class variables) are in keys.
+            # So the error happens for these cases:
+            #  (c) an instance where any of the keys come from its class instead of the instance itself
+            #  (d) an uninstantiated class being used for keys that are inherited rather than direct class variables
+            raise ValueError("%s inherits property %s. Treating it as dynamic could affect other objects."
+                             % (obj, key))
+    saved = {
+        key: getattr(obj, key)
+        for key in keys
+    }
+    try:
+        for key in keys:
+            setattr(obj, key, kwargs[key])
+        yield
+    finally:
+        for key in keys:
+            setattr(obj, key, saved[key])
