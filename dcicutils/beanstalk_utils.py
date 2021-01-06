62a06df4f18bec08ddceaa3e64ef96f9427fc9f8
Will Ronchetti <wrr33@cornell.edu>
2021-01-06 12:27:15 -0500

    Beanstalk Bugfix (#127)
    * pass region, parse API correctly
    
    * add changelog entries
    
    * take Kent's suggestion
    
    Co-authored-by: Kent M Pitman <netsettler@users.noreply.github.com>
    
    * use region constant
    
    Co-authored-by: Kent M Pitman <netsettler@users.noreply.github.com>

--- dcicutils/beanstalk_utils.py
+++ dcicutils/beanstalk_utils.py
@@ -393,9 +393,11 @@ def _get_beanstalk_configuration_settings(env):
             }
     """
     try:
-        client = boto3.client('elasticbeanstalk')
+        client = boto3.client('elasticbeanstalk', region_name=REGION)
         config = client.describe_configuration_settings(ApplicationName='4dn-web', EnvironmentName=env)
-        options = config['ConfigurationSettings']['OptionSettings']  # guaranteed to be present
+        # These are guaranteed to be present
+        [settings] = config['ConfigurationSettings']
+        options = settings['OptionSettings']
         return options
     except ClientError:
         logger.error('Error encountered attempting to get environment settings for %s' % env)
