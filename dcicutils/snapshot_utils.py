ce74d4c460faa4736f146456400392a012bd21d2
Kent Pitman <kent_pitman@hms.harvard.edu>
2021-03-19 21:26:51 -0400

    Move DEBUG_SNAPSHOTS into class.

--- dcicutils/snapshot_utils.py
+++ dcicutils/snapshot_utils.py
@@ -8,9 +8,6 @@ from dcicutils.misc_utils import (
 )
 
 
-# When this is more stable/trusted, we might want to remove this.
-# But I'd rather not be adding/removing instrumentation as I'm debugging it. -kmp 11-Mar-2021
-DEBUG_SNAPSHOTS = environ_bool("DEBUG_SNAPSHOTS", default=False)
 
 
 def is_valid_indexer_namespace(indexer_namespace, allow_null=False):
@@ -157,6 +154,10 @@ def es_data_cache(is_abstract=False, is_base=False):
 class ElasticSearchDataCache:
     """ Caches whether or not we have already provisioned a particular body of data. """
 
+    # When this is more stable/trusted, we might want to remove this.
+    # But I'd rather not be adding/removing instrumentation as I'm debugging it. -kmp 11-Mar-2021
+    DEBUG_SNAPSHOTS = environ_bool("DEBUG_SNAPSHOTS", default=False)
+
     SNAPSHOTS_INITIALIZED = {}
 
     repository_short_name = 'snapshots'
@@ -186,7 +187,7 @@ class ElasticSearchDataCache:
         :param level: This is used internally and should not be passed explicitly.  It helps with indentation
             and is used as a prefix when DEBUG_WORKBOOK_CACHE is True and is otherwise ignored.
         """
-        if DEBUG_SNAPSHOTS:
+        if cls.DEBUG_SNAPSHOTS:
             if level == 0:
                 PRINT()
             PRINT(level * "  ", level,
@@ -201,7 +202,7 @@ class ElasticSearchDataCache:
                                # initialized (or would be put explicitly back in order). We'll soon phase this out.
                                # -kmp 13-Feb-2021
                                only_on_first_call=True)
-        if DEBUG_SNAPSHOTS:
+        if cls.DEBUG_SNAPSHOTS:
             PRINT(level * "  ", level,
                   "Exiting %s.assure_data_once_loaded at %s" % (cls.__name__, datetime.datetime.now()))
 
@@ -232,7 +233,7 @@ class ElasticSearchDataCache:
         :param level: This is used internally and should not be passed explicitly.  It helps with indentation
             and is used as a prefix when DEBUG_WORKBOOK_CACHE is True and is otherwise ignored.
         """
-        if DEBUG_SNAPSHOTS:
+        if cls.DEBUG_SNAPSHOTS:
             if level == 0:
                 PRINT()
             PRINT(level * "  ", level, "Entering %s.assure_data_loaded at %s" % (cls.__name__, datetime.datetime.now()))
@@ -243,47 +244,47 @@ class ElasticSearchDataCache:
         if not cls.is_snapshot_initialized(snapshot_name):
             cls.load_data(es_testapp, datadir=datadir, indexer_namespace=indexer_namespace, other_data=other_data,
                           level=level + 1)
-            if DEBUG_SNAPSHOTS:
+            if cls.DEBUG_SNAPSHOTS:
                 PRINT(level * "  ", level, "Creating snapshot", snapshot_name, "at", datetime.datetime.now())
             create_workbook_snapshot(es_testapp,
                                      snapshots_repository_location=cls.snapshots_repository_location,
                                      repository_short_name=cls.repository_short_name,
                                      snapshot_name=snapshot_name)
             cls.mark_snapshot_initialized(snapshot_name)
-            if DEBUG_SNAPSHOTS:
+            if cls.DEBUG_SNAPSHOTS:
                 PRINT(level * "  ", level, "Done creating snapshot", snapshot_name, "at", datetime.datetime.now())
         elif only_on_first_call:
             # DEPRECATION NOTICE: This is legacy behavior related to how the 'workbook' fixture used to work,
             # which was to rely on loading it once and then rather than reverting it. -kmp 13-Feb-2021
-            if DEBUG_SNAPSHOTS:
+            if cls.DEBUG_SNAPSHOTS:
                 PRINT(level * "  ", level, "Skipping snapshot restoration because only_first_on_call=True.")
             pass
         else:
-            if DEBUG_SNAPSHOTS:
+            if cls.DEBUG_SNAPSHOTS:
                 PRINT(level * "  ", level, "Restoring snapshot", snapshot_name, "at", datetime.datetime.now())
             restore_workbook_snapshot(es_testapp,
                                       indexer_namespace=cls.indexer_namespace,
                                       repository_short_name=cls.repository_short_name,
                                       snapshot_name=snapshot_name)
-            if DEBUG_SNAPSHOTS:
+            if cls.DEBUG_SNAPSHOTS:
                 PRINT(level * "  ", level, "Done restoring snapshot", snapshot_name, "at", datetime.datetime.now())
 
-        if DEBUG_SNAPSHOTS:
+        if cls.DEBUG_SNAPSHOTS:
             PRINT(level * "  ", level, "Exiting %s.assure_data_loaded at %s" % (cls.__name__, datetime.datetime.now()))
 
     @classmethod
     def load_data(cls, es_testapp, datadir, indexer_namespace, other_data=None, level=0):
-        if DEBUG_SNAPSHOTS:
+        if cls.DEBUG_SNAPSHOTS:
             PRINT(level * "  ", level, "Entering %s.load_data at %s" % (cls.__name__, datetime.datetime.now()))
         if not is_data_cache(cls):
             raise RuntimeError("The class %s is not a registered data cache class."
                                " It may need an @es_data_cache() decoration."
                                % full_class_name(cls))
-        if DEBUG_SNAPSHOTS:
+        if cls.DEBUG_SNAPSHOTS:
             PRINT(level * "  ", level, "Checking ancestors of", cls.__name__)
         ancestor_found = None
         for ancestor_class in ancestor_classes(cls):
-            if DEBUG_SNAPSHOTS:
+            if cls.DEBUG_SNAPSHOTS:
                 PRINT(level * "  ", level, "Trying ancestor", ancestor_class)
             # We only care about classes that are descended from our root class, obeying our protocols,
             # and actually allowed to have snapshots made (i.e., not declared abstract). Other mixed in
@@ -309,33 +310,33 @@ class ElasticSearchDataCache:
                 else:
                     ancestor_found = ancestor_class
         if ancestor_found:
-            if DEBUG_SNAPSHOTS:
+            if cls.DEBUG_SNAPSHOTS:
                 PRINT(level * "  ", level, "Assuring data for ancestor class", ancestor_found.__name__,
                       "on behalf of", cls.__name__)
             ancestor_found.assure_data_loaded(es_testapp,
                                               datadir=datadir,
                                               indexer_namespace=indexer_namespace,
                                               other_data=other_data, level=level+1)
-            if DEBUG_SNAPSHOTS:
+            if cls.DEBUG_SNAPSHOTS:
                 PRINT(level * "  ", level, "Done assuring data for ancestor class", ancestor_found.__name__,
                       "on behalf of", cls.__name__)
         else:
-            if DEBUG_SNAPSHOTS:
+            if cls.DEBUG_SNAPSHOTS:
                 PRINT(level * "  ", level, "No useful ancestor found. No foundation to load.", cls.__name__)
         # Having built a foundation, now add the data that we wanted.
-        if DEBUG_SNAPSHOTS:
+        if cls.DEBUG_SNAPSHOTS:
             PRINT(level * "  ", level, "Loading additional requested class data", cls.__name__)
         # Now that a proper foundation is assured, load the new data that this class contributes.
         cls.load_additional_data(es_testapp, other_data=other_data)
-        if DEBUG_SNAPSHOTS:
+        if cls.DEBUG_SNAPSHOTS:
             PRINT(level * "  ", level, "Done loading additional requested class data", cls.__name__)
         # Finally, assure everything is indexed.
-        if DEBUG_SNAPSHOTS:
+        if cls.DEBUG_SNAPSHOTS:
             print(level * "  ", level, "Starting indexing at", datetime.datetime.now())
         es_testapp.post_json('/index', {'record': False})
-        if DEBUG_SNAPSHOTS:
+        if cls.DEBUG_SNAPSHOTS:
             print(level * "  ", level, "Done indexing at", datetime.datetime.now())
-        if DEBUG_SNAPSHOTS:
+        if cls.DEBUG_SNAPSHOTS:
             PRINT(level * "  ", level, "Exiting %s.load_data at %s" % (cls.__name__, datetime.datetime.now()))
 
     @classmethod
@@ -376,7 +377,7 @@ class ElasticSearchDataCache:
         for attr, value in attributes_and_values.items():
             existing = getattr(cls, attr)
             if existing and existing != value:
-                if DEBUG_SNAPSHOTS:
+                if cls.DEBUG_SNAPSHOTS:
                     import pdb
                     pdb.set_trace()
                 raise RuntimeError("Conflicting %s: %s (new) and %s (existing)." % (attr, value, existing))
