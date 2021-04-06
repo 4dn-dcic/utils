82ddd46801a9d82de090389590a291798fec0d94
Will Ronchetti <wrr33@cornell.edu>
2021-04-06 17:44:16 -0400

    C4-617 Docker, ECR, ECS Utils (#137)
    * C4-617 docker, ecr, ecs primitives
    
    * small changes
    
    * document authorization process
    
    * pass region
    
    * small changes
    
    * fix bug in uri resolution
    
    * stream push result
    
    * fix region arg
    
    * add auth config to push?
    
    * small changes

---
+++ test/test_ecr_utils.py
@@ -0,0 +1,44 @@
+import pytest
+from unittest import mock
+from dcicutils.ecr_utils import ECRUtils
+from dcicutils.docker_utils import DockerUtils
+
+
+REPO_URL = '123456789.dkr.ecr.us-east-2.amazonaws.com/cgap-mastertest'  # dummy URL
+
+
+def mocked_describe_respositories():
+    """ Mocks the important info from the describe_repositories API call. """
+    return {'repositories': [{
+        'repositoryUri': REPO_URL,
+       }]}
+
+
+def mocked_ecr_login(*, username, password, registry):
+    return
+
+
+def test_ecr_utils_basic():
+    """ Tests something simple for now, more tests to be added later. """
+    cli = ECRUtils()  # default args ok
+    with mock.patch.object(cli.client, 'describe_repositories', mocked_describe_respositories):
+        url = cli.resolve_repository_uri()
+        assert url == REPO_URL
+
+
+def test_ecr_utils_workflow():
+    """ Tests URL + Login via Docker_cli"""
+    ecr_cli = ECRUtils()
+    docker_cli = DockerUtils()
+    with mock.patch.object(ecr_cli.client, 'describe_repositories', mocked_describe_respositories):
+        ecr_cli.resolve_repository_uri()
+        ecr_user, ecr_pass = 'dummy', 'dummy'  # XXX: integrated test for this mechanism
+        with mock.patch.object(docker_cli.client, 'login', mocked_ecr_login):
+            docker_cli.login(ecr_repo_uri=ecr_cli.url,
+                             ecr_user=ecr_user, ecr_pass=ecr_pass)
+            # XXX: integrated test the remaining?
+
+
+def test_ecr_utils_integrated():
+    """ Write me! """
+    pass
