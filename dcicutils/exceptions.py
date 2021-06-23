c9a14ff9588be4be3f4ab8a3a01f432ee6c9010e
Kent Pitman <kent_pitman@hms.harvard.edu>
2021-06-23 06:01:10 -0400

    Add some exception classes.

--- dcicutils/exceptions.py
+++ dcicutils/exceptions.py
@@ -65,3 +65,54 @@ class UnexpectedErrorAfterFix(FixedBugError):
         super().__init__("An error of class %s was thrown where we expected one of class %s was fixed."
                          " This may be a regression in the fix for %s: %s"
                          % (full_class_name(error_seen), full_object_name(expected_class), self.bug_name, error_seen))
+
+
+class ConfigurationError(ValueError):
+    pass
+
+
+class InferredBucketConflict(ConfigurationError):
+
+    def __init__(self, *, kind, specified, inferred):
+        self.kind = kind
+        self.specified = specified
+        self.inferred = inferred
+        super().__init__("Specified {kind} bucket, {specified},"
+                         " and {kind} bucket inferred from health page, {inferred}, do not match."
+                         .format(kind=kind, specified=specified, inferred=inferred))
+
+class CannotInferEnvFromEmptyGlobal(ConfigurationError):
+
+    def __init__(self, *, global_bucket):
+        self.global_bucket = global_bucket
+        super().__init__("No envs were found in the global bucket, {global_bucket}, so no env can be inferred."
+                         .format(global_bucket=global_bucket))
+
+
+class CannotInferEnvFromManyGlobal(ConfigurationError):
+
+    def __init__(self, *, global_bucket, keys):
+        self.global_bucket = global_bucket
+        self.keys = keys
+        super().__init__("Too many keys were found in the global bucket, {global_bucket},"
+                         " for a particular env to be inferred: {keys}"
+                         .format(global_bucket=global_bucket, keys=keys))
+
+
+class CannotFindEnvInGlobal(ConfigurationError):
+
+    def __init__(self, *, global_bucket, keys, env):
+        self.global_bucket = global_bucket
+        self.keys = keys
+        self.env = env
+        super().__init__("No matches for global env bucket: {global_bucket}; keys: {keys}; desired env: {env}"
+                         .format(global_bucket=global_bucket, keys=keys, env=env))
+
+
+class GlobalBucketAccessError(ConfigurationError):
+
+    def __init__(self, *, global_bucket, status):
+        self.global_bucket = global_bucket
+        self.status = status
+        super().__init__("Could not access GLOBAL_BUCKET_ENV {global_bucket}: status: {status}"
+                         .format(global_bucket=global_bucket, status=status))
