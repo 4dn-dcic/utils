4a1ccdc3b0811c2a93b282888e89e1da9a3851df
Kent Pitman <kent_pitman@hms.harvard.edu>
2021-06-23 16:57:36 -0400

    Add some tests.

--- test/test_exceptions.py
+++ test/test_exceptions.py
@@ -1,9 +1,7 @@
 from dcicutils.exceptions import (
     ExpectedErrorNotSeen, WrongErrorSeen, UnexpectedErrorAfterFix, WrongErrorSeenAfterFix,
-    ConfigurationError, SynonymousEnvironmentVariablesMismatched,
-    # To be tested later...
-    # InferredBucketConflict,
-    # CannotInferEnvFromNoGlobalEnvs, CannotInferEnvFromManyGlobalEnvs, MissingGlobalEnv, GlobalBucketAccessError,
+    ConfigurationError, SynonymousEnvironmentVariablesMismatched, InferredBucketConflict,
+    CannotInferEnvFromNoGlobalEnvs, CannotInferEnvFromManyGlobalEnvs, MissingGlobalEnv, GlobalBucketAccessError,
 )
 
 
@@ -108,15 +106,58 @@ def test_configuration_error():
 
 def test_synonymous_environment_variables_mismatched():
     e = SynonymousEnvironmentVariablesMismatched(var1="twelve", val1="6+6", var2="dozen", val2="10+2")
-    assert str(e).startswith("The environment variables twelve and dozen are synonyms but have inconsistent values")
+    assert str(e) == ('The environment variables twelve and dozen are synonyms but have '
+                      'inconsistent values: If you supply values for both, they must be the same '
+                      "value. You supplied: twelve='6+6' dozen='10+2'")
     assert isinstance(e, SynonymousEnvironmentVariablesMismatched)
     assert isinstance(e, ConfigurationError)
     assert isinstance(e, ValueError)
     assert isinstance(e, Exception)
 
-# TODO: Write more tests later...
-#  InferredBucketConflict, subclass of ConfigurationError, with init args kind, specified, inferred.
-#  CannotInferEnvFromNoGlobalEnvs, subclass of ConfigurationError, with init arg global_bucket.
-#  CannotInferEnvFromManyGlobalEnvs, subclass of ConfigurationError, with init args global_bucket, keys.
-#  MissingGLobalEnv,subclass of ConfigurationError, with init args global_bucket, keys, env.
-#  GlobalBucketAccessError, subclass of ConfigurationError, with init args global_bucket, status.
+
+def test_inferred_bucket_conflict():
+    e = InferredBucketConflict(kind="foo", specified="abc", inferred="def")
+    assert str(e) == 'Specified foo bucket, abc, and foo bucket inferred from health page, def, do not match.'
+    assert isinstance(e, InferredBucketConflict)
+    assert isinstance(e, ConfigurationError)
+    assert isinstance(e, ValueError)
+    assert isinstance(e, Exception)
+
+
+def test_cannot_infer_env_from_no_global_envs():
+    e = CannotInferEnvFromNoGlobalEnvs(global_bucket="my-envs")
+    assert str(e) == 'No envs were found in the global env bucket, my-envs, so no env can be inferred.'
+    assert isinstance(e, CannotInferEnvFromNoGlobalEnvs)
+    assert isinstance(e, ConfigurationError)
+    assert isinstance(e, ValueError)
+    assert isinstance(e, Exception)
+
+
+def test_cannot_infer_env_from_many_global_envs():
+    e = CannotInferEnvFromManyGlobalEnvs(global_bucket="my-envs", keys=['myorg-prod', 'myorg-dev', 'myorg-test'])
+    assert str(e) == ("Too many keys were found in the global env bucket, my-envs,"
+                      " for a particular env to be inferred: ['myorg-prod', 'myorg-dev', 'myorg-test']")
+    assert isinstance(e, CannotInferEnvFromManyGlobalEnvs)
+    assert isinstance(e, ConfigurationError)
+    assert isinstance(e, ValueError)
+    assert isinstance(e, Exception)
+
+
+def test_missing_global_env():
+    e = MissingGlobalEnv(global_bucket="my-envs", keys=['myorg-prod', 'myorg-dev', 'myorg-test'], env='my-demo')
+    assert str(e) == ("No matches for global env bucket: my-envs;"
+                      " keys: ['myorg-prod', 'myorg-dev', 'myorg-test'];"
+                      " desired env: my-demo")
+    assert isinstance(e, MissingGlobalEnv)
+    assert isinstance(e, ConfigurationError)
+    assert isinstance(e, ValueError)
+    assert isinstance(e, Exception)
+
+
+def test_global_bucket_access_error():
+    e = GlobalBucketAccessError(global_bucket="my-envs", status=403)
+    assert str(e) == "Could not access global env bucket my-envs: status: 403"
+    assert isinstance(e, GlobalBucketAccessError)
+    assert isinstance(e, ConfigurationError)
+    assert isinstance(e, ValueError)
+    assert isinstance(e, Exception)
