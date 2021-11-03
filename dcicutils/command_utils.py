2e5849ea5cdf3403eaa3aea12d46bca42b283622
Kent Pitman <kent_pitman@hms.harvard.edu>
2021-11-03 16:09:30 -0400

    Scripting technology for downloading repositories and activating a virtual env.

--- dcicutils/command_utils.py
+++ dcicutils/command_utils.py
@@ -1,7 +1,12 @@
 import contextlib
+import glob
 import logging
+import os
+import requests
 import subprocess
 
+from dcicutils.exceptions import InvalidParameterError
+from dcicutils.lang_utils import there_are
 from typing import Optional
 from .misc_utils import PRINT
 
@@ -77,7 +82,7 @@ class ShellScript:
     # This is the shell the script will use to execute
     EXECUTABLE = "/bin/bash"
 
-    def __init__(self, executable: Optional[str] = None, simulate=False, **script_options):
+    def __init__(self, executable: Optional[str] = None, simulate=False, no_execute: bool = False, **script_options):
         """
         Creates an object that will let you compose a script and eventually execute it as a subprocess.
 
@@ -89,6 +94,7 @@ class ShellScript:
             raise ValueError(f"Unknown script_options supplied: {script_options}")
         self.executable = executable or self.EXECUTABLE
         self.simulate = simulate
+        self.no_execute = no_execute
         self.script = ""
 
     def do_first(self, command: str):
@@ -139,13 +145,27 @@ class ShellScript:
 
     def execute(self, **pipe_args):
         """This is where it's really executed."""
-        if self.simulate:
-            PRINT("SIMULATED:")
-            PRINT("=" * 80)
-            PRINT(self.script.replace('; ', ';\\\n '))
-            PRINT("=" * 80)
-        elif self.script:
-            return subprocess.run(self.script, shell=True, executable=self.executable, **pipe_args)
+
+        # Well, it MIGHT be really executed. The no_execute option can fully suppress that so
+        # that the commands are accumulated for delayed execution. Not even simulation is done
+        # here in that case. (e.g., see the 'done_first' context manager)
+        # But this option is subprimitive. Most users will want 'simulate'.
+        # -kmp 2-Nov-2021
+
+        if not self.no_execute:  # fully suppresses execution. see rationale above
+            if self.simulate:
+                PRINT("SIMULATED:")
+                PRINT("=" * 80)
+                PRINT(self.script.replace('; ', ';\\\n '))
+                PRINT("=" * 80)
+            elif self.script:
+                return subprocess.run(self.script, shell=True, executable=self.executable, **pipe_args)
+
+    @contextlib.contextmanager
+    def done_first(self):
+        with shell_script(script_class=self.__class__, no_execute=True) as script_segment:
+            yield script_segment
+            self.do_first(script_segment.script)
 
 
 @contextlib.contextmanager
@@ -160,6 +180,9 @@ def shell_script(working_dir=None, executable=None, simulate=False, script_class
             script.do("pwd")  # should print out /some/working/dir
 
     """
+    # Note that we deliberately don't offer the 'no_execute' option in the arguemnt list,
+    # since it can be provided in **script_options in the few cases where it's needed,
+    # and to not distract from 'the 'simulate' option which is probably what users usually want.  -kmp 2-Nov-2021
     script = script_class(executable=executable, simulate=simulate, **script_options)
     if working_dir:
         with script.using_working_dir(working_dir):
@@ -205,3 +228,81 @@ def module_warnings_as_ordinary_output(module):
     finally:
         # This operation is safe even if the filter didn't get as far as being added.
         logger.removeFilter(just_print_text)
+
+C4_PROJECTS = ["dbmi-bgm", "4dn-dcic"]
+
+def guess_c4_git_url(c4_repo, all_projects=None):
+    for project in all_projects or C4_PROJECTS:
+        base_url = f"https://github.com/{project}/{c4_repo}"
+        response = requests.get(base_url)
+        if response.status_code == 200:
+            return base_url + ".git"
+    raise ValueError(f"Unknown C4 repository: {c4_repo}")
+
+
+def setup_subrepo(subrepo_name, subrepo_git_url=None, parent_root=None,
+                  branch='master', simulate=False, parent_repos_subdir='repositories',
+                  all_projects=None):
+
+    if parent_root is None:
+        parent_root = os.path.abspath(os.curdir)
+    if subrepo_git_url is None:
+        subrepo_git_url = guess_c4_git_url(c4_repo=subrepo_name, all_projects=all_projects)
+
+    all_subrepos_path = os.path.join(parent_root, parent_repos_subdir)
+    subrepo_path = os.path.join(all_subrepos_path, subrepo_name)
+    venv_name = subrepo_name + "_env"
+
+    if not os.path.exists(all_subrepos_path):
+        print(f"Creating {all_subrepos_path}...")
+        os.mkdir(all_subrepos_path)
+
+    if not os.path.exists(subrepo_path):
+        with shell_script(working_dir=all_subrepos_path, simulate=simulate) as script:
+            script.do(f'echo "Cloning {subrepo_name}..."')
+            script.do(f'git clone {subrepo_git_url} {subrepo_name}')
+            script.pushd(subrepo_name)
+            script.do(f'git checkout {branch}')
+            script.popd()
+            script.do(f'echo "Done cloning {subrepo_path}."')
+    else:
+
+        with shell_script(working_dir=subrepo_path, simulate=simulate) as script:
+            script.do(f'echo "Pulling latest changes for {subrepo_name} in branch {branch} ..."')
+            script.do(f'git checkout {branch}')
+            script.do(f'git pull')
+            script.do(f'echo "Done pulling latest changes for {subrepo_name}."')
+
+    with shell_script(working_dir=subrepo_path, simulate=simulate) as script:
+        script_assure_venv(repo_path=subrepo_path, script=script, default_venv_name=venv_name)
+
+
+def script_assure_venv(repo_path, script, default_venv_name, method='poetry'):
+
+    venv_names = glob.glob(os.path.join(repo_path, "*env"))
+    n_venv_names = len(venv_names)
+    if n_venv_names == 0:
+        venv_name = default_venv_name
+        script.do(f'echo "Creating virtual environment {venv_name}."')
+        script.do(f'pyenv exec python -m venv {venv_name} || python3 -m venv {venv_name}')
+        script.do(f'source {venv_name}/bin/activate')
+        script.do(f'echo "The installation process for {venv_name} may take about 3-5 minutes. Please be patient."')
+        script.do(f'echo "Starting installation of {venv_name} requirements at $(date)."')
+        if method == 'setup':
+            script.do(f'echo "Using setup.py to install requirements for virtual environment {venv_name}."')
+            script.do(f'python setup.py develop')
+        elif method == 'poetry':
+            script.do(f'echo "Using poetry to install requirements for virtual environment {venv_name}."')
+            script.do(f'poetry install')
+        elif method == 'requirements':
+            script.do(f'echo "Using requirements to install requirements for virtual environment {venv_name}."')
+            script.do(f'pip install -r requirements.txt')
+        else:
+            raise InvalidParameterError(parameter='method', value=method, options=['setup', 'requirements', 'poetry'])
+        script.do(f'echo "Finished installation of {venv_name} requirements at $(date)."')
+    elif n_venv_names == 1:
+        [venv_name] = venv_names
+        script.do(f'echo "Activating existing virtual environment {venv_name}."')
+        script.do(f'source {venv_name}/bin/activate')
+    else:
+        raise RuntimeError(there_are(venv_names, kind="virtual environment"))
