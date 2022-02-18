4cf2850ffbfee368de41258dd3704a6e3838a3c0
Kent Pitman <kent_pitman@hms.harvard.edu>
2022-02-18 13:39:00 -0500

    Add some doc string responsive to code review.

--- dcicutils/secrets_utils.py
+++ dcicutils/secrets_utils.py
@@ -66,7 +66,20 @@ def assume_identity():
 # However, the examples are poorly written and involve way too much weird state. -kmp 16-Feb-2022
 
 class SecretsTable:
-    """Assists with certain read-only operations on a given secret in the SecertsManager."""
+    """
+    Assists with certain read-only operations on a given secret in the SecertsManager.
+
+    This can be created with the name of what the AWS SecretsManager calls a secret,
+    and what we call an identity. The idea is to allow read-only access to that data
+    in tabular form.
+
+    Once created, this table can be accessed via .as_dict() to turn it into a regular
+    dictionary, but it can also be accessed via secrets_table.get(entryname) or
+    secretstable[entryname] to access individual entries in the table. See tests for examples.
+
+    NOTE: We could later extend this to allow creating or modifying the table as well,
+    but that might involve extra permissions and be less broadly useful.
+    """
     def __init__(self, name, secretsmanager_client=None, stage=None):
         """
         :param name: The SecretId of the secret to help with.
@@ -80,9 +93,20 @@ class SecretsTable:
         self.stage = stage
 
     def __str__(self):
+        """
+        Returns a string summarizing the table by name.
+
+        :return: a string for use by str to describe this object as <SecretsTable 'name' @nnnn>
+        """
         return f"<{full_class_name(self)} {self.name!r} @{'%x' % id(self)}>"
 
     def __repr__(self):
+        """
+        Returns a non-rereadable string summarizing the table by name.
+
+        :return: a string for use by repr to describe this object as <SecretsTable 'name' @nnnn>,
+                 the same as str does.
+        """
         return self.__str__()
 
     def _get_secret_value(self):
@@ -108,14 +132,30 @@ class SecretsTable:
             return response
 
     def _get_secret_string(self):
-        # This might return None if there is no string secret. We exepct that in o ur case there always will be.
-        # Note that binary secrets are in SecretBinary instead of SecretString, but we don't use those.
+        """
+        The secrets held by the AWS SecretsManager are actually strings or binary blobs.
+        The strings are often JSON representing detailed entries in a table. This just
+        gets the big string, in the case that it's string data (the only case we care about)
+        so is mostly an internal thing. No user should be calling this.
+
+        Note that this MIGHT return None if there is no string secret, usually because there was
+        binary data instead. We expect that in our case there always will be string data in SecretString,
+        but it would be in SecretBinary instead if there was binary data.
+        """
         result = self._get_secret_value().get('SecretString')
         if result is None:
             raise ValueError(f"There is no SecretString for named {self.name!r}.")
         return result
 
     def as_dict(self):
+        """
+        Returns a representation of the secret as a Python dict.
+
+        Note that calling this function gives the equivalent to what secrets_utils.assume_identity returns.
+        See the tests for an example.
+
+        :return: a dict representing the secrets table
+        """
         secret_string = self._get_secret_string()
         if secret_string:
             try:
@@ -124,9 +164,26 @@ class SecretsTable:
                 raise ValueError(f"Unable to parse SecretString named {self.name} as a dictionary.")
 
     def get(self, item, default=None):
+        """
+        Returns the value of a given entry in the SecretsTable, or a default value (default None) otherwise.
+
+        :item: a string naming the entry.
+        :default: a default to use if the entry is missing (default None)
+
+        :return: a string or None
+        """
         return self.as_dict().get(item, default)
 
     def __getitem__(self, item):
+        """
+        Returns the value of a given entry in the SecretsTable, which is presumed to exist (or an error is raised).
+
+        :item: a string naming the entry.
+
+        :return: a string
+
+        :raises KeyError: if the entry does not exist
+        """
         result = self.get(item)
         if not result:
             raise KeyError(item)
@@ -135,10 +192,23 @@ class SecretsTable:
 
     @classmethod
     def _all_secrets(cls, *, secretsmanager_client):
+        """
+        The AWS SecretsManager is itself a table. This gets a list of metadata dictionaries for each item in the
+        SecretsManager, each of which can be the name of what we're here calling a SecretsTable. The name of
+        each such metadata dictionary, the name of the associated secret, is in the dict entry called 'Name'.
+
+        :return: a list of metadata dictionaries
+        """
         return secretsmanager_client.list_secrets()['SecretList']
 
     @classmethod
     def _find_secrets(cls, *, pattern, secretsmanager_client):
+        """
+        This is like ._all_secrets() but returns a list of metadata dictionaries that correspond only to those
+        whose names match the given regexp pattern.
+
+        :return: a list of metadata dictionaries
+        """
         results = []
         for secret in cls._all_secrets(secretsmanager_client=secretsmanager_client):
             if re.search(pattern, secret['Name']):
@@ -147,6 +217,15 @@ class SecretsTable:
 
     @classmethod
     def _find_secret(cls, *, pattern, secretsmanager_client):
+        """
+        This is like ._all_secrets() but returns exactly one metadata dictionary (not in a list)
+        that has a name matching the given regexp pattern. It is an error if there are too many
+        or none at all.
+
+        :return: a metadata dictionary
+
+        :raises RuntimeError: if zero matching dictionaries or more than one matching dictionaries are found
+        """
         results = cls._find_secrets(secretsmanager_client=secretsmanager_client, pattern=pattern)
         n = len(results)
         if n == 0:
@@ -158,12 +237,36 @@ class SecretsTable:
 
     @classmethod
     def find_secret(cls, pattern, secretsmanager_client=None):
+        """
+        This returns metadata about a secret with a name matching the given pattern.
+
+        NOTE: This is like ._find_secret() but is not subprimitive. It is intended for calling by client interfaces,
+        so it has nicer defaulting of arguments.
+
+        :return: a metadata dictionary
+
+        :raises RuntimeError: if zero matching dictionaries or more than one matching dictionaries are found
+        """
+
         secretsmanager_client = secretsmanager_client or boto3.client('secretsmanager')
         return cls._find_secret(secretsmanager_client=secretsmanager_client, pattern=pattern)
 
     @classmethod
     def find_application_secrets_table(cls, secretsmanager_client=None,
                                        application_configuration_pattern='ApplicationConfiguration'):
+        """
+        This tries to find a secret representing an ApplicationConfiguration, since often there is only one.
+        If there are multiple ApplicationConfigurations present, it may be necessary to specify a more specific
+        search pattern via application_configuration_pattern=.
+
+        :param secretsmanager_client: A boto3 client for secretsmanager. If one is not suppllied, it will be created.
+           But it is sometimes good to reuse an existing one if you have one handy.
+        :param application_configuration_pattern: A search pattern to use if there is intended to be be more than one
+           application configuration to select among. The default should be fine if there is only one, although
+           this somewhat depends on how 4dn-cloud-infra orchestration names things.
+
+        :return: a SecretsTable representing the SecretString in the secret that is found by the search
+        """
         secretsmanager_client = secretsmanager_client or boto3.client('secretsmanager')
         secret = cls.find_secret(pattern=application_configuration_pattern,
                                  secretsmanager_client=secretsmanager_client)
--- test/test_secrets_utils.py
+++ test/test_secrets_utils.py
@@ -8,20 +8,28 @@ from dcicutils.secrets_utils import assume_identity, SecretsTable
 from unittest import mock
 
 
-# some_secret_table = {"foo": "the foo thing", "bar": "the bar thing"}
 some_secret_names = ['foo', 'bar']
 some_secret_values = ['the {item} thing' for item in some_secret_names]
+# in other words, some_secret_table = {"foo": "the foo thing", "bar": "the bar thing"}
 some_secret_table = dict(zip(some_secret_names, some_secret_values))
 some_secret_string = json.dumps(some_secret_table)
+# The common identity pattern is for use in tests that want to find several things that have the same common substring,
+# but that don't match the full set.
 some_common_identity_substring = "SecretIdentityToken"
 some_common_identity_pattern = "Secret.*Token"
 some_unique_secret_identity_token = "Mastertest"
 some_secret_identity = (f'Mocked{some_unique_secret_identity_token}'
                         f'ApplicationConfiguration{some_common_identity_substring}')
+# These decoys are because sometimes a SecretsManager has more than just an application configuration in it.
+# For example, 4dn-cloud-infra separately manages the secrets associated with RDS into other data that could
+# likewise be accessed via a SecretsTable.  Testing assumes that we're lookning for an ApplicationConfiguration,
+# but that's an arbitrary choice.
 decoy_1_string = '{"decoy": 1}'
 decoy_1_identity = f'MockedDecoy1Something{some_common_identity_substring}'
 decoy_2_string = '{"decoy": 2}'
+# This search string will find exactly one secret that is not our application configuration for testing.
 some_unique_decoy_token = "UniqueThing"
+# This search string will find exactly one secret that is not our application configuration for testing.
 some_unique_decoy_name = f'MockedDecoy2{some_unique_decoy_token}'
 decoy_2_identity = some_unique_decoy_name
 some_secret_identities_with_common_pattern = [some_secret_identity, decoy_1_identity]
@@ -39,11 +47,11 @@ def boto3_for_some_secrets_testing():
 
 def test_assume_identity():
 
-    b3 = boto3_for_some_secrets_testing()
+    b3 = boto3_for_some_secrets_testing()  # this sets things up with some_secret_string in the SecretsManager
     with mock.patch.object(secrets_utils_module, 'boto3', b3):
         with override_environ(IDENTITY=some_secret_identity):
             identity = assume_identity()
-            assert identity == some_secret_table
+            assert identity == some_secret_table  # this is the parsed form of some_secret_string
 
 
 def test_secrets_table_as_dict():
@@ -71,7 +79,8 @@ def test_mock_secrets_table_as_dict_and_assume_identity_equivalence():
 def test_mock_secrets_table_str_and_repr():
 
     with mock.patch.object(secrets_utils_module, "id") as mocked_id:
-        some_id = 12345678
+        # We can't control the actual memory address to test the printer, so we have to mock 'id' to lie about it.
+        some_id = 12345678  # this will be the pretend address of our object
         hex_id = "%x" % some_id
         assert hex_id == 'bc614e'
         mocked_id.return_value = some_id
@@ -194,6 +203,8 @@ def test_secrets_table_find_secret():
     b3 = boto3_for_some_secrets_testing()
     with mock.patch.object(secrets_utils_module, 'boto3', b3):
         secrets_table = SecretsTable.find_application_secrets_table()
+        # The '_alt' thing here is us testing we can call with or without a secretsmanager_client.
+        # The choice is purely a matter of reusing resources for efficiency.
         found_secret = SecretsTable.find_secret(pattern=some_unique_decoy_token)
         found_secret_alt = SecretsTable.find_secret(secretsmanager_client=secrets_table.secretsmanager_client,
                                                     pattern=some_unique_decoy_token)
@@ -214,22 +225,14 @@ def test_find_application_secrets_table():
     b3 = boto3_for_some_secrets_testing()
     with mock.patch.object(secrets_utils_module, 'boto3', b3):
 
-        with pytest.raises(RuntimeError):  # there are too many for this to succeed unassisted
-            found = SecretsTable.find_application_secrets_table(application_configuration_pattern='Decoy')
-            ignored(found)  # Shouldn't reach here
+        found_table = SecretsTable.find_application_secrets_table()  # in our examples, there's only one
+        assert found_table.name == some_secret_identity
 
         found_table = SecretsTable.find_application_secrets_table(
             application_configuration_pattern=some_unique_secret_identity_token)
         assert found_table.name == some_secret_identity
 
-        found_table = SecretsTable.find_application_secrets_table()  # in our examples, there's only one
-        assert found_table.name == some_secret_identity
-
-
-#     @classmethod
-#     def find_application_secrets_table(cls, secretsmanager_client=None):
-#         secretsmanager_client = secretsmanager_client or boto3.client('secretsmanager')
-#         secret = cls.find_secret(substring='ApplicationConfiguration',
-#                                  secretsmanager_client=secretsmanager_client)
-#         secret_name = secret['Name']
-#         return SecretsTable(name=secret_name, secretsmanager_client=secretsmanager_client)
+        with pytest.raises(RuntimeError):  # there are too many for this to succeed unassisted
+            # There is no thing called DecoyApplicationConfiguration
+            found = SecretsTable.find_application_secrets_table(application_configuration_pattern='Decoy')
+            ignored(found)  # Shouldn't reach here
