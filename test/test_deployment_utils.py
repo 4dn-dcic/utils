b36f8be95656d4229227ae031f9a3f824d591254
Kent Pitman <kent_pitman@hms.harvard.edu>
2022-07-02 03:48:53 -0400

    Fix some deployment_utils tests: Use new fresh_ff_state context manager. Test actual env names. In test_get_deployment_config_ff_prd_orchestrated, use a more consistent env name for a cgap test. In cgap stg test, skip if stg is None.

--- test/test_deployment_utils.py
+++ test/test_deployment_utils.py
@@ -11,6 +11,7 @@ import sys
 from io import StringIO
 from unittest import mock
 
+from dcicutils import deployment_utils as deployment_utils_module
 from dcicutils.deployment_utils import (
     IniFileManager, boolean_setting, CreateMappingOnDeployManager,
     BasicOrchestratedCGAPIniFileManager, BasicLegacyCGAPIniFileManager,
@@ -18,12 +19,15 @@ from dcicutils.deployment_utils import (
     # TODO: This isn't yet tested.
     # EBDeployer,
 )
-from dcicutils.env_utils import is_cgap_env, is_hotseat_env, is_test_env, data_set_for_env, EnvUtils
+from dcicutils.env_utils import is_cgap_env, is_hotseat_env, is_test_env, data_set_for_env, EnvUtils, full_env_name
 from dcicutils.exceptions import InvalidParameterError
 from dcicutils.qa_utils import MockFileSystem, printed_output, MockedCommandArgs
 from dcicutils.misc_utils import ignored, file_contents
 from dcicutils.qa_utils import override_environ
-from .helpers import fresh_cgap_state
+from .helpers import (
+    fresh_cgap_state, fresh_ff_state,  # fresh_legacy_state,
+    using_fresh_ff_state,  # using_fresh_legacy_state, using_fresh_cgap_state,
+)
 
 
 _MY_DIR = os.path.dirname(__file__)
@@ -1274,20 +1278,21 @@ def _get_deploy_config(*, env, args=None, log=None, allow_other_prod=False):
 def test_get_deployment_config_cgap_stg_orchestrated():
     """ Tests get_deployment_config in a fourfront staging situation. """
     with fresh_cgap_state():
-        my_log = MockedInfoLog()
-        cfg = _get_deploy_config(env=EnvUtils.STG_ENV_NAME, log=my_log)
-        assert cfg['ENV_NAME'] == EnvUtils.STG_ENV_NAME  # sanity
-        assert cfg['SKIP'] is False
-        assert cfg['WIPE_ES'] is True
-        assert cfg['STRICT'] is True
-        assert my_log.last_msg == (f"Environment {EnvUtils.STG_ENV_NAME} is currently the staging environment."
-                                   f" Processing mode: STRICT,WIPE_ES")
+        if EnvUtils.STG_ENV_NAME is not None:
+            my_log = MockedInfoLog()
+            cfg = _get_deploy_config(env=EnvUtils.STG_ENV_NAME, log=my_log)
+            assert cfg['ENV_NAME'] == EnvUtils.STG_ENV_NAME  # sanity
+            assert cfg['SKIP'] is False
+            assert cfg['WIPE_ES'] is True
+            assert cfg['STRICT'] is True
+            assert my_log.last_msg == (f"Environment {EnvUtils.STG_ENV_NAME} is currently the staging environment."
+                                       f" Processing mode: STRICT,WIPE_ES")
 
 
 @pytest.mark.integrated
 def test_get_deployment_config_ff_stg_orchestrated():
     """ Tests get_deployment_config in a fourfront staging situation. """
-    with EnvUtils.fresh_ff_state():
+    with fresh_ff_state():
         my_log = MockedInfoLog()
         cfg = _get_deploy_config(env=EnvUtils.STG_ENV_NAME, log=my_log)
         assert cfg['ENV_NAME'] == EnvUtils.STG_ENV_NAME  # sanity
@@ -1315,7 +1320,7 @@ def test_get_deployment_config_cgap_prd_orchestrated():
 @pytest.mark.integrated
 def test_get_deployment_config_ff_prd_orchestrated():
     """ Tests get_deployment_config in the new production case """
-    with EnvUtils.fresh_ff_state():
+    with fresh_ff_state():
         my_log = MockedInfoLog()
         cfg = _get_deploy_config(env=EnvUtils.PRD_ENV_NAME, log=my_log)
         assert cfg['ENV_NAME'] == EnvUtils.PRD_ENV_NAME  # sanity
@@ -1333,7 +1338,7 @@ def test_get_deployment_config_cgap_uncorrelated_stg_or_prd_orchestrated():
         # In the new world, this actually never happens because only EnvUtils.PRD_ENV_NAME and EnvUtils.STG_ENV_NAME
         # are possible stg-or-prd envs, but the control flow is there, so this tests it.
         with mock.patch('dcicutils.deployment_utils.is_stg_or_prd_env', return_value=True):
-            my_env = 'fourfront-prd-other'
+            my_env = 'cgap-prd-other'
             my_log = MockedInfoLog()
             with pytest.raises(RuntimeError):
                 _get_deploy_config(env=my_env, log=my_log)
@@ -1345,7 +1350,7 @@ def test_get_deployment_config_cgap_uncorrelated_stg_or_prd_orchestrated():
 @pytest.mark.integrated
 def test_get_deployment_config_ff_uncorrelated_stg_or_prd_orchestrated():
     """ Tests get_deployment_config in the new production case """
-    with EnvUtils.fresh_ff_state():
+    with fresh_ff_state():
         # In the new world, this actually never happens because only EnvUtils.PRD_ENV_NAME and EnvUtils.STG_ENV_NAME
         # are possible stg-or-prd envs, but the control flow is there, so this tests it.
         with mock.patch('dcicutils.deployment_utils.is_stg_or_prd_env', return_value=True):
@@ -1378,7 +1383,7 @@ def test_get_deployment_config_cgap_test_envs_orchestrated():
 @pytest.mark.integrated
 def test_get_deployment_config_ff_test_envs_orchestrated():
     """ Tests get_deployment_config in the mastertest case with an orchestrated ecosystem. """
-    with EnvUtils.fresh_ff_state():
+    with fresh_ff_state():
         for my_env in EnvUtils.TEST_ENVS:
             if not is_hotseat_env(my_env):
                 print(f"Testing {my_env}...")
@@ -1425,7 +1430,7 @@ def test_get_deployment_config_cgap_hotseat_orchestrated():
 @pytest.mark.integrated
 def test_get_deployment_config_ff_hotseat_orchestrated():
     """ Tests get_deployment_config in the hotseat case with an orchestrated ecosystem. """
-    with EnvUtils.fresh_ff_state():
+    with fresh_ff_state():
         for my_env in EnvUtils.HOTSEAT_ENVS:
             print(f"Testing {my_env}...")
             my_log = MockedInfoLog()
@@ -1450,6 +1455,71 @@ def test_get_deployment_config_ff_hotseat_orchestrated():
                 assert cfg['STRICT'] is False
                 assert my_log.last_msg == (f'Environment {my_env} is an unrecognized environment.'
                                            f' Processing mode: default')
+
+
+@using_fresh_ff_state()
+@mock.patch.object(deployment_utils_module, "compute_ff_prd_env")
+@mock.patch.object(deployment_utils_module, "compute_cgap_prd_env")
+def test_actual_env_settings(mock_compute_cgap_prd_env, mock_compute_ff_prd_env):
+    def dont_compute_cagp(*args, **kwargs):
+        ignored(args, kwargs)
+        raise AssertionError("Should not be trying to call compute_cgap_prd_env")
+    mock_compute_cgap_prd_env.side_effect = dont_compute_cagp
+    mock_compute_ff_prd_env.return_value = 'fourfront-prd'
+
+    assert EnvUtils.PRD_ENV_NAME == 'fourfront-prd'
+    assert EnvUtils.STG_ENV_NAME == 'fourfront-stg'
+
+    def describe(config):
+        return ('SKIP'
+                if config['SKIP'] else
+                ",".join((['WIPE_ES'] if config['WIPE_ES'] else []) +
+                         (['STRICT'] if config['STRICT'] else [])))
+
+    class MyCommandArgs(MockedCommandArgs):
+        VALID_ARGS = ['skip', 'wipe_es', 'strict']
+
+    def compute_and_show(kind, env, wipe_es=False):
+        info = "-------------------------"
+        try:
+            config = _get_deploy_config(env=env, args=MyCommandArgs(wipe_es=wipe_es))
+            info = describe(config).ljust(14)
+        finally:  # show even if we err
+            print(f"{info} env={env.ljust(20)} wipe_es={'Y' if wipe_es else 'N'}"
+                  f" full={full_env_name(env).ljust(20)} kind={kind}")
+        return config
+
+    print()  # Start on a fresh line
+    for wipe_es in [False, True]:
+        print(f"wipe_es={wipe_es}")
+        for kind_and_envs in [['production', ['fourfront-prd', 'prd', 'data']],
+                              ['staging', ['fourfront-stg', 'stg', 'staging']],
+                              ['hotseat', ['fourfront-hotseat', 'hotseat']],
+                              ['test', ['fourfront-mastertest', 'mastertest', 'fourfront-webdev', 'webdev']],
+                              ['other', ['fourfront-other', 'other']]]:
+            [kind, envs] = kind_and_envs
+            for env in envs:
+
+                config = compute_and_show(kind=kind, env=env, wipe_es=wipe_es)
+
+                if kind == 'production':
+                    assert config['SKIP'] is False
+                    assert config['WIPE_ES'] is False
+                    assert config['STRICT'] is True
+                elif kind == 'staging':
+                    assert config['SKIP'] is False
+                    assert config['WIPE_ES'] is True
+                    assert config['STRICT'] is True
+                elif kind == 'hotseat':
+                    assert config['SKIP'] is True
+                elif kind == 'test':
+                    assert config['SKIP'] is False
+                    assert config['WIPE_ES'] is True
+                    assert config['STRICT'] is False
+                else:  # other
+                    assert config['SKIP'] is False
+                    assert config['WIPE_ES'] is wipe_es
+                    assert config['STRICT'] is False
 
 
 def test_create_file_from_template():
