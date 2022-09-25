bce60a5bed1ae61b5383a1538d16b73d84cb259d
Kent Pitman <kent_pitman@hms.harvard.edu>
2022-09-25 00:27:18 -0400

    Initialize EnvUtils to sample data if NO_SERVER_FIXTURES set.

--- conftest.py
+++ conftest.py
@@ -5,3 +5,5 @@ _account_number = os.environ.get('ACCOUNT_NUMBER')
 if _account_number and _account_number != '643366669028':
     raise Exception(f"These tests must be run with legacy credentials."
                     f"Your credentials are set to account {_account_number}")
+
+
