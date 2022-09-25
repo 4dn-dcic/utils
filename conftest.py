6dc65519279ac9abe0846c50dc93e1e66ff31cf7
Kent Pitman <kent_pitman@hms.harvard.edu>
2022-09-25 02:20:01 -0400

    Conditionalize on the right env variable.

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from dcicutils.env_utils import EnvUtils
 from dcicutils.misc_utils import environ_bool, PRINT
 
 
-NO_SERVER_FIXTURES = environ_bool("NO_SERVER_FIXTURES")
+USE_SAMPLE_ENVUTILS = environ_bool("USE_SAMPLE_ENVUTILS")
 
 
 _account_number = os.environ.get('ACCOUNT_NUMBER')
@@ -13,6 +13,6 @@ if _account_number and _account_number != '643366669028':
     raise Exception(f"These tests must be run with legacy credentials."
                     f"Your credentials are set to account {_account_number}")
 
-if NO_SERVER_FIXTURES:
+if USE_SAMPLE_ENVUTILS:
     PRINT(f"EnvUtils using sample configuration template.")
     EnvUtils.set_declared_data(data=EnvUtils.SAMPLE_TEMPLATE_FOR_CGAP_TESTING)
