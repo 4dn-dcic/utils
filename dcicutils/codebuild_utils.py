c68cacf98805e7662fb7ed8d7edd660b56bb3579
William Ronchetti <wrr33@cornell.edu>
2022-12-05 12:05:38 -0500

    add override run

--- dcicutils/codebuild_utils.py
+++ dcicutils/codebuild_utils.py
@@ -17,3 +17,17 @@ class CodeBuildUtils:
     def run_project_build(self, *, project_name: str) -> dict:
         """ Runs the given project build. """
         return self.client.start_build(projectName=project_name)
+
+    def run_project_build_with_overrides(self, *, project_name, branch, env_overrides):
+        """ Runs the given project build with branch and environment variable overrides """
+        return self.client.start_build(
+            projectName=project_name,
+            sourceVersion=branch,
+            environmentVariablesOverride=[
+                {
+                    'name': k,
+                    'value': v,
+                    'type': 'PLAINTEXT'
+                } for k, v in env_overrides.items()
+            ]
+        )
