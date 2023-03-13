c2a40044c1d631cc5b65d9840fce70c4899659e3
Kent Pitman <kent_pitman@hms.harvard.edu>
2023-03-13 17:28:45 -0400

    Undo expectation that prod is fixed for C4-853.

--- test/test_s3_utils.py
+++ test/test_s3_utils.py
@@ -898,13 +898,13 @@ def test_s3_utils_buckets_modern():
 
 C4_853_FIX_INFO = {
     'fourfront-production-blue':
-        {'metadata_fix': True, 'tibanna_fix': False},
+        {'metadata_fix': False, 'tibanna_fix': False},
     'fourfront-production-green':
-        {'metadata_fix': True, 'tibanna_fix': False},
+        {'metadata_fix': False, 'tibanna_fix': False},
     'data':
-        {'metadata_fix': True, 'tibanna_fix': False},
+        {'metadata_fix': False, 'tibanna_fix': False},
     'staging':
-        {'metadata_fix': True, 'tibanna_fix': False},
+        {'metadata_fix': False, 'tibanna_fix': False},
 
     'fourfront-mastertest':
         {'metadata_fix': True, 'tibanna_fix': False},
