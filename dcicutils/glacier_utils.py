aed21e3f8de39854031595a290010a0f243392c1
William Ronchetti <wrr33@cornell.edu>
2023-04-14 11:56:57 -0400

    refactor slightly

--- dcicutils/glacier_utils.py
+++ dcicutils/glacier_utils.py
@@ -72,14 +72,14 @@ class GlacierUtils:
             PRINT(f'Error restoring object {key} from Glacier storage class: {str(e)}')
             return None
 
-    def extract_temporary_s3_location_from_restore_response(self, bucket: str, key: str) -> Union[str, None]:
+    def extract_temporary_s3_location_from_restore_response(self, bucket: str, key: str) -> Union[dict, None]:
         """ Extracts the S3 location that the restored file will be sent to given the bucket and key
 
         :param bucket: bucket of original file location
         :param key: key of original file location
-        :return: path or None
+        :return: Restore object from s3
         """
-        try:
+        try:  # extract temporary location by heading object
             response = self.s3.head_object(Bucket=bucket, Key=key)
             restore = response.get('Restore')
             if restore is None:
@@ -92,37 +92,23 @@ class GlacierUtils:
             if restore_path is None:
                 PRINT(f'Error: Could not determine the temporary location of restored object {bucket}/{key}')
                 return None
-            return restore_path
+            return restore
         except Exception as e:
             PRINT(f'Error copying object {bucket}/{key} back to its original location in S3: {str(e)}')
             return None
 
-    def copy_object_back_to_original_location(self, bucket: str, key: str, restore_path: Union[str, None] = None,
+    def copy_object_back_to_original_location(self, bucket: str, key: str,
                                               version_id: Union[str, None] = None) -> Union[bool, None]:
         """ Reads the temporary location from the restored object and copies it back to the original location
 
         :param bucket: bucket where object is stored
         :param key: key within bucket where object is stored
-        :param restore_path: path to which the object was restored, can be empty
         :param version_id: version of object, if applicable
         :return: boolean whether the copy was successful
         """
         try:
-            if not restore_path:
-                # Get the restored object's temporary location by heading the existing object
-                response = self.s3.head_object(Bucket=bucket, Key=key)
-                restore = response.get('Restore')
-                if restore is None:
-                    PRINT(f'Object {bucket}/{key} is not currently being restored from Glacier')
-                    return False
-                if 'ongoing-request="false"' not in restore:
-                    PRINT(f'Object {bucket}/{key} is still being restored from Glacier')
-                    return False
-                restore_path = response.get('RestoreOutputPath')
-                if restore_path is None:
-                    PRINT(f'Error: Could not determine the temporary location of restored object {bucket}/{key}')
-                    return False
-
+            # Get temporary location
+            restore = self.extract_temporary_s3_location_from_restore_response(bucket, key)
             # Copy the object from the temporary location to its original location
             copy_source = {'Bucket': bucket, 'Key': key, 'Restore': restore}
             copy_args = {'Bucket': bucket, 'Key': key}
