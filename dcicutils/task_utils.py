aca12bc2d9dce38de13200c13c72eda35d809116
Kent Pitman <kent_pitman@hms.harvard.edu>
2023-04-16 21:23:44 -0400

    Readjust for generator behavior.

--- dcicutils/task_utils.py
+++ dcicutils/task_utils.py
@@ -1,12 +1,12 @@
 import threading
 
 from dcicutils.exceptions import MultiError
-from dcicutils.misc_utils import check_true, environ_bool, PRINT
+from dcicutils.lang_utils import n_of
+from dcicutils.misc_utils import chunked, environ_bool, PRINT
 
 # TODO:
 #  * timeoouts
-#  * chunking
-#  * revised error handling for chunking
+#  * does not kill threads on abort. they have to run their course.
 
 PMAP_VERBOSE = environ_bool("PMAP_VERBOSE")
 
@@ -45,20 +45,32 @@ class TaskManager:
 
     TASK_CLASS = Task
 
-    MAX_CONCURRENT_THREADS = 10
+    DEFAULT_CHUNK_SIZE = 10
 
-    def __init__(self, fail_fast=True, raise_error=True):
+    def __init__(self, fail_fast=True, raise_error=True, chunk_size=None):
         if fail_fast and not raise_error:
             raise ValueError("raise_erorr cannot be false if fail_fast is true.")
         self.fail_fast = fail_fast
         self.raise_error = raise_error
-
-    _ARG_TYPE = (list, tuple)
-    _ARG_TYPE_ERROR_MESSAGE = "Each arguments to pmap must be a list or a tuple."
-    _ARG_LEN_ERROR_MESSAGE = "All arguments to pmap must be of the same length."
+        self.chunk_size = chunk_size or self.DEFAULT_CHUNK_SIZE
 
     @classmethod
-    def pmap(cls, fn, seq, *more_seqs, fail_fast=True, raise_error=True):
+    def pmap(cls, fn, seq, *more_seqs, fail_fast=True, raise_error=True, chunk_size=None):
+        for chunk in cls.map_chunks(fn, seq, *more_seqs, fail_fast=fail_fast, raise_error=raise_error,
+                                    chunk_size=chunk_size):
+            for item in chunk:
+                yield item
+
+    @classmethod
+    def pmap_list(cls, fn, seq, *more_seqs, fail_fast=True, raise_error=True, chunk_size=None):
+        result = []
+        for chunk in cls.map_chunks(fn, seq, *more_seqs, fail_fast=fail_fast, raise_error=raise_error,
+                                    chunk_size=chunk_size):
+            result += chunk
+        return result
+
+    @classmethod
+    def map_chunks(cls, fn, seq, *more_seqs, fail_fast=True, raise_error=True, chunk_size=None):  # generator
         """
         Maps a function across given arguments with each call performed in a separate thread.
         If errors occur, they are trapped and appropriate information is communicated to the main thread.
@@ -72,39 +84,55 @@ class TaskManager:
         :param more_seqs: Additional sequences (seq2, seq3, ...) of arguments if fn requires (arg2, arg3, ...)
         :param fail_fast: Whether to stop as soon as an error is noticed.
         :param raise_error: Whether to raise errors that are detected. If false, the error object become results.
+        :param chunk_size: How many items to do at once. If unsupplied, the .DEFAULT_CHUNK_SIZE will be used.
         """
-        return TaskManager(fail_fast=fail_fast, raise_error=raise_error)._do_map(fn, seq, *more_seqs)
+        manager = TaskManager(fail_fast=fail_fast, raise_error=raise_error, chunk_size=chunk_size)
+        return manager._map_chunks(fn, seq, *more_seqs)
 
-    def _do_map(self, fn, seq1, *more_seqs):
-        check_true(isinstance(seq1, self._ARG_TYPE), self._ARG_TYPE_ERROR_MESSAGE, error_class=ValueError)
-        check_true(all(isinstance(seq, self._ARG_TYPE) for seq in more_seqs), self._ARG_TYPE_ERROR_MESSAGE,
-                   error_class=ValueError)
-        n = len(seq1)
-        check_true(all(len(seq) == n for seq in more_seqs), self._ARG_LEN_ERROR_MESSAGE, error_class=ValueError)
-        records = [Task(manager=self, position=i, function=fn, arg1=seq1[i],
-                        more_args=[seq[i] for seq in more_seqs])
-                   for i in range(n)]
-        for record in records:
-            record.thread = threading.Thread(target=lambda x: x.call(), args=(record,))
-        for record in records:
-            if PMAP_VERBOSE:  # pragma: no cover
-                PRINT(f"Starting {record.thread} for arg {record.position}...")
-            record.thread.start()  # make sure they all start
-        for record in records:
-            if PMAP_VERBOSE:
-                PRINT(f"Joining {record.thread} for arg {record.position}...")
-            record.thread.join()
-            if record.error and self.fail_fast:
-                raise record.error
-        if not self.raise_error:
-            return list(map(lambda record: record.error or record.result, records))
-        errors = [record.error for record in records if record.ready and record.error]
-        if not errors:
-            return [record.result for record in records]
-        elif len(errors) == 1:
-            raise errors[0]
-        else:
-            raise MultiError(*errors)
+    def _map_chunks(self, fn, seq1, *more_seqs):
+        chunk_post = 0
+        more_seq_generators = [(x for x in seq) for seq in more_seqs]
+        for chunk in chunked(seq1, chunk_size=self.chunk_size):
+            n = len(chunk)
+            records = [Task(manager=self, position=chunk_post + i, function=fn, arg1=chunk_element,
+                            more_args=[next(seq_generator) for seq_generator in more_seq_generators])
+                       for i, chunk_element in enumerate(chunk)]
+            for record in records:
+                record.thread = threading.Thread(target=lambda x: x.call(), args=(record,))
+            for record in records:
+                if PMAP_VERBOSE:  # pragma: no cover
+                    PRINT(f"Starting {record.thread} for arg {record.position}...")
+                record.thread.start()  # make sure they all start
+            for record in records:
+                if PMAP_VERBOSE:
+                    PRINT(f"Joining {record.thread} for arg {record.position}...")
+                record.thread.join()
+                if record.error and self.fail_fast:
+                    if PMAP_VERBOSE:
+                        PRINT(f"While accumulating records, an error was found and is being raised due to fail_fast.")
+                    raise record.error
+            if not self.raise_error:
+                if PMAP_VERBOSE:
+                    PRINT(f"Because fail_fast is false, returning list of {n_of(n, 'error or result')}"
+                          f" for chunk {chunk}.")
+                yield list(map(lambda record: record.error or record.result, records))
+            else:
+                errors = [record.error for record in records if record.ready and record.error]
+                if not errors:
+                    if PMAP_VERBOSE:
+                        PRINT(f"No errors to raise in chunk {chunk}.")
+                    yield [record.result for record in records]
+                elif len(errors) == 1:
+                    if PMAP_VERBOSE:
+                        PRINT(f"Just one error to raise in chunk {chunk}.")
+                    raise errors[0]
+                else:
+                    if PMAP_VERBOSE:
+                        PRINT(f"Multiple errors to raise as a MultiError in chunk {chunk}.")
+                    raise MultiError(*errors)
+            chunk_post += n
 
 
+map_chunks = TaskManager.map_chunks
 pmap = TaskManager.pmap
+pmap_list = TaskManager.pmap_list
--- test/test_task_utils.py
+++ test/test_task_utils.py
@@ -1,7 +1,8 @@
 import pytest
 
 from dcicutils.exceptions import MultiError
-from dcicutils.task_utils import Task, TaskManager, pmap
+from dcicutils.misc_utils import print_error_message
+from dcicutils.task_utils import Task, TaskManager, map_chunks, pmap, pmap_list
 
 
 def _add1(x):
@@ -41,51 +42,123 @@ def test_TaskManager():
     assert manager.raise_error
 
 
-def test_pmap():
+def test_map_chunks_simple():
 
-    with pytest.raises(Exception):
-        pmap(_add1)  # noQA - testing that the list of sequences is required.
+    for chunk_size in [2, 3]:
+        i = 1
+        n_chunks = 0
+        for chunk in map_chunks(_add1, [1, 2, 3, 4, 5, 6], chunk_size=chunk_size):
+            n_chunks += 1
+            assert isinstance(chunk, list)
+            assert len(chunk) == chunk_size
+            for elem in chunk:
+                i += 1
+                assert elem == i
+        assert n_chunks == 6 / chunk_size
 
-    assert pmap(_add1, [1, 2, 3]) == [2, 3, 4]
 
-    with pytest.raises(Exception):
-        pmap(_adder, [1, 2, 3], [1, 2])  # noQA - testing arg length mismatch
+def test_pmap_simple():
 
-    assert pmap(_adder, [1, 2, 3], [4, 5, 6]) == [5, 7, 9]
+    print()
+    res = pmap(_add1, [1, 2, 3])
+    print(f"res={res}")
+    assert not isinstance(res, list)
+    assert next(res) == 2
+    assert next(res) == 3
+    assert next(res) == 4
+    assert next(res, None) is None
 
 
-def test_pmap_raise():
+def test_pmap_list_simple():
+
+    assert pmap_list(_add1, [1, 2, 3]) == [2, 3, 4]
+
+    assert pmap_list(_adder, [1, 2, 3], [4, 5, 6]) == [5, 7, 9]
+
+
+def test_pmap_list_raise():
 
     with pytest.raises(_BadArgument):
-        pmap(_add1, [1, 'a'])
+        pmap_list(_add1, [1, 'a'])
 
     with pytest.raises(_BadArgument):
-        pmap(_add1, ['a', 'b'], fail_fast=True)
+        pmap_list(_add1, ['a', 'b'], fail_fast=True)
 
     with pytest.raises(MultiError) as exc:
-        pmap(_add1, ['a', 'b'], fail_fast=False)
+        pmap_list(_add1, ['a', 'b'], fail_fast=False)
     e0, e1 = exc.value.errors
     assert isinstance(e0, _BadArgument)  # same error as before
     assert isinstance(e1, _BadArgument)  # another of same
     assert e0 != e1
 
 
-def test_pmap_no_raise():
+def test_pmap_list_no_raise():
 
     with pytest.raises(Exception) as exc:
-        pmap(_add1, [], raise_error=False, fail_fast=True)
+        pmap_list(_add1, [], raise_error=False, fail_fast=True)
     assert str(exc.value) == "raise_erorr cannot be false if fail_fast is true."
 
-    r1, r2 = pmap(_add1, [1, 'a'], raise_error=False, fail_fast=False)
+    r1, r2 = pmap_list(_add1, [1, 'a'], raise_error=False, fail_fast=False)
     assert r1 == 2
     assert isinstance(r2, _BadArgument)
 
-    r1, r2 = pmap(_add1, ['a', 'b'], raise_error=False, fail_fast=False)
+    r1, r2 = pmap_list(_add1, ['a', 'b'], raise_error=False, fail_fast=False)
     assert isinstance(r1, _BadArgument)
     assert isinstance(r2, _BadArgument)
 
 
-def test_pmap_fail_fast():
+def test_pmap_list_no_raise_chunked():
 
-    with pytest.raises(_BadArgument):
-        pmap(_add1, ['a', 'b'], fail_fast=True)  # just reports the first error it finds
+    with pytest.raises(Exception) as exc:
+        pmap_list(_add1, [], raise_error=False, fail_fast=True)
+    assert str(exc.value) == "raise_erorr cannot be false if fail_fast is true."
+
+    # These should behave the same for raise_error=False, fail_fast=False
+    for chunk_size in [2, 3]:
+        r1a, r1b, r2a, r2b, r3a, r3b, r4a = pmap_list(_add1, [1, 2, 3, 'a', 'b', 6, 'c'],
+                                                      raise_error=False, fail_fast=False, chunk_size=chunk_size)
+        assert r1a == 2
+        assert r1b == 3
+        assert r2a == 4
+        assert isinstance(r2b, _BadArgument)
+        assert isinstance(r3a, _BadArgument)
+        assert r3b == 7
+        assert isinstance(r4a, _BadArgument)
+
+
+def test_pmap_list_raise_fast_chunked():
+
+    # These should behave the same for raise_error=True, fail_fast=True
+    for chunk_size in [2, 3]:
+        with pytest.raises(Exception) as exc:
+            pmap_list(_add1, [1, 2, 3, 'a', 'b', 6, 'c'],
+                      raise_error=True, fail_fast=True, chunk_size=chunk_size)
+        error_object = exc.value
+        assert isinstance(error_object, _BadArgument)
+
+
+def test_pmap_list_raise_slow_chunked():
+
+    # The behavior for raise_error=True, fail_fast=False will be different for chunk_size=2 and chunk_size=3
+    # For chunk_size=2, there will be one error for 'a', so it will be raised as a regular error.
+    with pytest.raises(Exception) as exc:
+        pmap_list(_add1, [1, 2, 3, 'a', 'b', 6, 'c'],
+                  raise_error=True, fail_fast=False, chunk_size=2)
+    error_object = exc.value
+    assert isinstance(error_object, _BadArgument)
+    # For chunk_size=3, there will be two errors for 'a' and 'b', so they will be raised as a MultiError
+    with pytest.raises(Exception) as exc:
+        pmap_list(_add1, [1, 2, 3, 'a', 'b', 6, 'c'],
+                  raise_error=True, fail_fast=False, chunk_size=3)
+    error_object = exc.value
+    assert isinstance(error_object, MultiError)
+    assert all(isinstance(e, _BadArgument) for e in error_object.errors)
+
+
+def test_pmap_list_fail_fast():
+
+    with pytest.raises(_BadArgument) as exc:
+        pmap_list(_add1, ['a', 'b'], fail_fast=True)  # just reports the first error it finds
+    e = exc.value
+    print_error_message(e)
+    assert isinstance(e, _BadArgument)  # Note that we did not wait for a MultiError
