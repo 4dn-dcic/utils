fd1ed9a6d4e2fe6bd1e313ee3e4bcf44f426fd4e
David Michaels <david_michaels@hms.harvard.edu>
2023-07-05 14:20:57 -0400

    Fix for (smaht) for test_common.py - this fix needs to go in master too

--- test/test_common.py
+++ test/test_common.py
@@ -1,9 +1,9 @@
-from dcicutils.common import EnvName, OrchestratedApp, APP_CGAP, APP_FOURFRONT, ORCHESTRATED_APPS
+from dcicutils.common import EnvName, OrchestratedApp, APP_CGAP, APP_FOURFRONT, APP_SMAHT, ORCHESTRATED_APPS
 
 
 def test_app_constants():
 
-    assert set(ORCHESTRATED_APPS) == {APP_CGAP, APP_FOURFRONT} == {'cgap', 'fourfront'}
+    assert set(ORCHESTRATED_APPS) == {APP_CGAP, APP_FOURFRONT, APP_SMAHT} == {'cgap', 'fourfront', 'smaht'}
 
     # For thexe next two, which are really type hints, just test that they exist.
     assert EnvName
