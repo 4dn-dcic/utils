a6b68feeb2219eb9f79635a7441656b09f38dc32
Kent Pitman <kent_pitman@hms.harvard.edu>
2023-08-16 13:33:06 -0400

    Rearrange some methods for presentational reasons.

--- dcicutils/sheet_utils.py
+++ dcicutils/sheet_utils.py
@@ -14,6 +14,120 @@ ParsedHeaders = List[ParsedHeader]
 SheetCellValue = Union[int, float, str]
 
 
+class ItemTools:
+    """
+    Implements operations on table-related data without pre-supposing the specific representation of the table.
+    It is assumed this can be used for data that was obtained from .json, .csv, .tsv, and .xlsx files because
+    it does not presuppose the source of the data nor where it will be written to.
+
+    For the purpose of this class:
+
+    * a 'header' is a string representing the top of a column.
+
+    * a 'parsed header' is a list of strings and/or ints, after splitting at uses of '#' or '.', so that
+      "a.b.c" is represented as ["a", "b", "c"], and "x.y#0" is represented as ["x", "y", 0], and representing
+      each numeric token as an int instead of a string.
+
+    * a 'headers' object is just a list of strings, each of which is a 'header'.
+
+    * a 'parsed headers' object is a non-empty list of lists, each of which is a 'parsed header'.
+      e..g., the headers ["a.b.c", "x.y#0"] is represented as parsed hearders [["a", "b", "c"], ["x", "y", 0]].
+
+   """
+
+    @classmethod
+    def parse_sheet_header(cls, header: Header) -> ParsedHeader:
+        result = []
+        token = ""
+        for i in range(len(header)):
+            ch = header[i]
+            if ch == '.' or ch == '#':
+                if token:
+                    result.append(int(token) if token.isdigit() else token)
+                    token = ""
+            else:
+                token += ch
+        if token:
+            result.append(int(token) if token.isdigit() else token)
+        return result
+
+    @classmethod
+    def parse_sheet_headers(cls, headers: Headers):
+        return [cls.parse_sheet_header(header)
+                for header in headers]
+
+    @classmethod
+    def compute_patch_prototype(cls, parsed_headers: ParsedHeaders):
+        prototype = {}
+        for parsed_header in parsed_headers:
+            parsed_header0 = parsed_header[0]
+            if isinstance(parsed_header0, int):
+                raise ValueError(f"A header cannot begin with a numeric ref: {parsed_header0}")
+            cls.assure_patch_prototype_shape(parent=prototype, keys=parsed_header)
+        return prototype
+
+    @classmethod
+    def assure_patch_prototype_shape(cls, *, parent: Union[Dict, List], keys: ParsedHeader):
+        [key0, *more_keys] = keys
+        key1 = more_keys[0] if more_keys else None
+        if isinstance(key1, int):
+            placeholder = []
+        elif isinstance(key1, str):
+            placeholder = {}
+        else:
+            placeholder = None
+        if isinstance(key0, int):
+            n = len(parent)
+            if key0 == n:
+                parent.append(placeholder)
+            elif key0 > n:
+                raise Exception("Numeric items must occur sequentially.")
+        elif isinstance(key0, str):
+            if key0 not in parent:
+                parent[key0] = placeholder
+        if key1 is not None:
+            cls.assure_patch_prototype_shape(parent=parent[key0], keys=more_keys)
+        return parent
+
+    @classmethod
+    def parse_value(cls, value: SheetCellValue) -> AnyJsonData:
+        if isinstance(value, str):
+            lvalue = value.lower()
+            # TODO: We could consult a schema to make this less heuristic, but this may do for now
+            if lvalue == 'true':
+                return True
+            elif lvalue == 'false':
+                return False
+            elif lvalue == 'null' or lvalue == '':
+                return None
+            elif '|' in value:
+                return [cls.parse_value(subvalue) for subvalue in value.split('|')]
+            else:
+                ch0 = value[0]
+                if ch0 == '+' or ch0 == '-' or ch0.isdigit():
+                    try:
+                        return int(value)
+                    except Exception:
+                        pass
+                    try:
+                        return float(value)
+                    except Exception:
+                        pass
+                return value
+        else:  # presumably a number (int or float)
+            return value
+
+    @classmethod
+    def set_path_value(cls, datum: Union[List, Dict], path: ParsedHeader, value: Any, force: bool = False):
+        if (value is None or value == '') and not force:
+            return
+        [key, *more_path] = path
+        if not more_path:
+            datum[key] = value
+        else:
+            cls.set_path_value(datum[key], more_path, value)
+
+
 class WorkbookManager:
 
     @classmethod
@@ -50,6 +164,12 @@ class WorkbookManager:
                               for col in self._all_cols(sheet)]
         self.headers_by_sheetname[sheet.title] = headers
 
+    def _load_row(self, *, sheet: Worksheet, row: int):
+        headers = self.sheet_headers(sheet.title)
+        row_dict: Dict[str, Any] = {headers[col-1]: sheet.cell(row=row, column=col).value
+                                    for col in self._all_cols(sheet)}
+        return row_dict
+
     def load_content(self):
         workbook: Workbook = load_workbook(self.filename)
         self.workbook = workbook
@@ -62,126 +182,6 @@ class WorkbookManager:
                 content.append(row_dict)
             self.content_by_sheetname[sheetname] = content
         return self.content_by_sheetname
-
-    def _load_row(self, *, sheet: Worksheet, row: int):
-        headers = self.sheet_headers(sheet.title)
-        row_dict: Dict[str, Any] = {headers[col-1]: sheet.cell(row=row, column=col).value
-                                    for col in self._all_cols(sheet)}
-        return row_dict
-
-
-class ItemTools:
-    """
-    Implements operations on table-related data without pre-supposing the specific representation of the table.
-    It is assumed this can be used for data that was obtained from .json, .csv, .tsv, and .xlsx files because
-    it does not presuppose the source of the data nor where it will be written to.
-
-    For the purpose of this class:
-
-    * a 'header' is a string representing the top of a column.
-
-    * a 'parsed header' is a list of strings and/or ints, after splitting at uses of '#' or '.', so that
-      "a.b.c" is represented as ["a", "b", "c"], and "x.y#0" is represented as ["x", "y", 0], and representing
-      each numeric token as an int instead of a string.
-
-    * a 'headers' object is just a list of strings, each of which is a 'header'.
-
-    * a 'parsed headers' object is a non-empty list of lists, each of which is a 'parsed header'.
-      e..g., the headers ["a.b.c", "x.y#0"] is represented as parsed hearders [["a", "b", "c"], ["x", "y", 0]].
-
-   """
-
-    @classmethod
-    def compute_patch_prototype(cls, parsed_headers: ParsedHeaders):
-        prototype = {}
-        for parsed_header in parsed_headers:
-            parsed_header0 = parsed_header[0]
-            if isinstance(parsed_header0, int):
-                raise ValueError(f"A header cannot begin with a numeric ref: {parsed_header0}")
-            cls.assure_patch_prototype_shape(parent=prototype, keys=parsed_header)
-        return prototype
-
-    @classmethod
-    def assure_patch_prototype_shape(cls, *, parent: Union[Dict, List], keys: ParsedHeader):
-        [key0, *more_keys] = keys
-        key1 = more_keys[0] if more_keys else None
-        if isinstance(key1, int):
-            placeholder = []
-        elif isinstance(key1, str):
-            placeholder = {}
-        else:
-            placeholder = None
-        if isinstance(key0, int):
-            n = len(parent)
-            if key0 == n:
-                parent.append(placeholder)
-            elif key0 > n:
-                raise Exception("Numeric items must occur sequentially.")
-        elif isinstance(key0, str):
-            if key0 not in parent:
-                parent[key0] = placeholder
-        if key1 is not None:
-            cls.assure_patch_prototype_shape(parent=parent[key0], keys=more_keys)
-        return parent
-
-    @classmethod
-    def parse_sheet_headers(cls, headers: Headers):
-        return [cls.parse_sheet_header(header)
-                for header in headers]
-
-    @classmethod
-    def parse_sheet_header(cls, header: Header) -> ParsedHeader:
-        result = []
-        token = ""
-        for i in range(len(header)):
-            ch = header[i]
-            if ch == '.' or ch == '#':
-                if token:
-                    result.append(int(token) if token.isdigit() else token)
-                    token = ""
-            else:
-                token += ch
-        if token:
-            result.append(int(token) if token.isdigit() else token)
-        return result
-
-    @classmethod
-    def set_path_value(cls, datum: Union[List, Dict], path: ParsedHeader, value: Any, force: bool = False):
-        if (value is None or value == '') and not force:
-            return
-        [key, *more_path] = path
-        if not more_path:
-            datum[key] = value
-        else:
-            cls.set_path_value(datum[key], more_path, value)
-
-    @classmethod
-    def parse_value(cls, value: SheetCellValue) -> AnyJsonData:
-        if isinstance(value, str):
-            lvalue = value.lower()
-            # TODO: We could consult a schema to make this less heuristic, but this may do for now
-            if lvalue == 'true':
-                return True
-            elif lvalue == 'false':
-                return False
-            elif lvalue == 'null' or lvalue == '':
-                return None
-            elif '|' in value:
-                return [cls.parse_value(subvalue) for subvalue in value.split('|')]
-            else:
-                ch0 = value[0]
-                if ch0 == '+' or ch0 == '-' or ch0.isdigit():
-                    try:
-                        return int(value)
-                    except Exception:
-                        pass
-                    try:
-                        return float(value)
-                    except Exception:
-                        pass
-                return value
-        else:  # presumably a number (int or float)
-            return value
 
 
 class ItemManager(ItemTools, WorkbookManager):
--- test/test_sheet_utils.py
+++ test/test_sheet_utils.py
@@ -1,29 +1,29 @@
 import os
 import pytest
 
-from dcicutils.sheet_utils import WorkbookManager, ItemManager
+from dcicutils.sheet_utils import ItemTools, WorkbookManager, ItemManager
 from .conftest_settings import TEST_DIR
 
 
-def test_item_manager_parse_sheet_header():
-    assert ItemManager.parse_sheet_header('.a') == ['a']
-    assert ItemManager.parse_sheet_header('a') == ['a']
-    assert ItemManager.parse_sheet_header('#0') == [0]
-    assert ItemManager.parse_sheet_header('0') == [0]
-    assert ItemManager.parse_sheet_header('foo.bar') == ['foo', 'bar']
-    assert ItemManager.parse_sheet_header('a.b#0') == ['a', 'b', 0]
-    assert ItemManager.parse_sheet_header('x.xx#17#8.z') == ['x', 'xx', 17, 8, 'z']
+def test_item_tools_parse_sheet_header():
+    assert ItemTools.parse_sheet_header('.a') == ['a']
+    assert ItemTools.parse_sheet_header('a') == ['a']
+    assert ItemTools.parse_sheet_header('#0') == [0]
+    assert ItemTools.parse_sheet_header('0') == [0]
+    assert ItemTools.parse_sheet_header('foo.bar') == ['foo', 'bar']
+    assert ItemTools.parse_sheet_header('a.b#0') == ['a', 'b', 0]
+    assert ItemTools.parse_sheet_header('x.xx#17#8.z') == ['x', 'xx', 17, 8, 'z']
 
     # We don't error-check this, but it shouldn't matter
-    assert ItemManager.parse_sheet_header('#abc') == ['abc']
-    assert ItemManager.parse_sheet_header('.123') == [123]
-    assert ItemManager.parse_sheet_header('#abc.123#456.def') == ['abc', 123, 456, 'def']
+    assert ItemTools.parse_sheet_header('#abc') == ['abc']
+    assert ItemTools.parse_sheet_header('.123') == [123]
+    assert ItemTools.parse_sheet_header('#abc.123#456.def') == ['abc', 123, 456, 'def']
 
 
-def test_item_manager_parse_sheet_headers():
+def test_item_tools_parse_sheet_headers():
     input = ['a.b', 'a.c', 'a.d#1', 'a.d#2']
     expected = [['a', 'b'], ['a', 'c'], ['a', 'd', 1], ['a', 'd', 2]]
-    assert ItemManager.parse_sheet_headers(input) == expected
+    assert ItemTools.parse_sheet_headers(input) == expected
 
 
 @pytest.mark.parametrize('parsed_headers,expected_prototype', [
@@ -38,32 +38,67 @@ def test_item_manager_parse_sheet_headers():
     (['a.b', 'a.c', 'a.d#0.foo', 'a.d#0.bar', 'a.d#1.foo', 'a.d#1.bar'],
      {'a': {'b': None, 'c': None, 'd': [{'foo': None, 'bar': None}, {'foo': None, 'bar': None}]}}),
 ])
-def test_item_manager_compute_patch_prototype(parsed_headers, expected_prototype):
-    parsed_headers = ItemManager.parse_sheet_headers(parsed_headers)
-    assert ItemManager.compute_patch_prototype(parsed_headers) == expected_prototype
+def test_item_tools_compute_patch_prototype(parsed_headers, expected_prototype):
+    parsed_headers = ItemTools.parse_sheet_headers(parsed_headers)
+    assert ItemTools.compute_patch_prototype(parsed_headers) == expected_prototype
 
 
 @pytest.mark.parametrize('headers', [['0'], ['x', '0.y']])
-def test_item_manager_compute_patch_prototype_errors(headers):
+def test_item_tools_compute_patch_prototype_errors(headers):
 
-    parsed_headers = ItemManager.parse_sheet_headers(headers)
+    parsed_headers = ItemTools.parse_sheet_headers(headers)
     with pytest.raises(ValueError) as exc:
-        ItemManager.compute_patch_prototype(parsed_headers)
+        ItemTools.compute_patch_prototype(parsed_headers)
     assert str(exc.value) == "A header cannot begin with a numeric ref: 0"
 
 
-def test_item_manager_set_path_value():
+def test_item_tools_parse_value():
+
+    for x in [37, 19.3, True, False, None, 'simple text']:
+        assert ItemTools.parse_value(x) == x
+
+    assert ItemTools.parse_value('3') == 3
+    assert ItemTools.parse_value('+3') == 3
+    assert ItemTools.parse_value('-3') == -3
+
+    assert ItemTools.parse_value('3.5') == 3.5
+    assert ItemTools.parse_value('+3.5') == 3.5
+    assert ItemTools.parse_value('-3.5') == -3.5
+
+    assert ItemTools.parse_value('3.5e1') == 35.0
+    assert ItemTools.parse_value('+3.5e1') == 35.0
+    assert ItemTools.parse_value('-3.5e1') == -35.0
+
+    assert ItemTools.parse_value('') is None
+
+    assert ItemTools.parse_value('null') is None
+    assert ItemTools.parse_value('Null') is None
+    assert ItemTools.parse_value('NULL') is None
+
+    assert ItemTools.parse_value('true') is True
+    assert ItemTools.parse_value('True') is True
+    assert ItemTools.parse_value('TRUE') is True
+
+    assert ItemTools.parse_value('false') is False
+    assert ItemTools.parse_value('False') is False
+    assert ItemTools.parse_value('FALSE') is False
+
+    assert ItemTools.parse_value('alpha|beta|gamma') == ['alpha', 'beta', 'gamma']
+    assert ItemTools.parse_value('alpha|true|false|null||7|1.5') == ['alpha', True, False, None, None, 7, 1.5]
+
+
+def test_item_tools_set_path_value():
 
     x = {'foo': 1, 'bar': 2}
-    ItemManager.set_path_value(x, ['foo'], 3)
+    ItemTools.set_path_value(x, ['foo'], 3)
     assert x == {'foo': 3, 'bar': 2}
 
     x = {'foo': [11, 22, 33], 'bar': {'x': 'xx', 'y': 'yy'}}
-    ItemManager.set_path_value(x, ['foo', 1], 17)
+    ItemTools.set_path_value(x, ['foo', 1], 17)
     assert x == {'foo': [11, 17, 33], 'bar': {'x': 'xx', 'y': 'yy'}}
 
     x = {'foo': [11, 22, 33], 'bar': {'x': 'xx', 'y': 'yy'}}
-    ItemManager.set_path_value(x, ['bar', 'x'], 'something')
+    ItemTools.set_path_value(x, ['bar', 'x'], 'something')
     assert x == {'foo': [11, 22, 33], 'bar': {'x': 'something', 'y': 'yy'}}
 
 
@@ -143,41 +178,6 @@ def test_workbook_manager_load_csv():
 
     with pytest.raises(Exception):
         WorkbookManager.load_workbook(SAMPLE_CSV_FILE)
-
-
-def test_item_manager_parse_value():
-
-    for x in [37, 19.3, True, False, None, 'simple text']:
-        assert ItemManager.parse_value(x) == x
-
-    assert ItemManager.parse_value('3') == 3
-    assert ItemManager.parse_value('+3') == 3
-    assert ItemManager.parse_value('-3') == -3
-
-    assert ItemManager.parse_value('3.5') == 3.5
-    assert ItemManager.parse_value('+3.5') == 3.5
-    assert ItemManager.parse_value('-3.5') == -3.5
-
-    assert ItemManager.parse_value('3.5e1') == 35.0
-    assert ItemManager.parse_value('+3.5e1') == 35.0
-    assert ItemManager.parse_value('-3.5e1') == -35.0
-
-    assert ItemManager.parse_value('') is None
-
-    assert ItemManager.parse_value('null') is None
-    assert ItemManager.parse_value('Null') is None
-    assert ItemManager.parse_value('NULL') is None
-
-    assert ItemManager.parse_value('true') is True
-    assert ItemManager.parse_value('True') is True
-    assert ItemManager.parse_value('TRUE') is True
-
-    assert ItemManager.parse_value('false') is False
-    assert ItemManager.parse_value('False') is False
-    assert ItemManager.parse_value('FALSE') is False
-
-    assert ItemManager.parse_value('alpha|beta|gamma') == ['alpha', 'beta', 'gamma']
-    assert ItemManager.parse_value('alpha|true|false|null||7|1.5') == ['alpha', True, False, None, None, 7, 1.5]
 
 
 def test_item_manager_load_content():
