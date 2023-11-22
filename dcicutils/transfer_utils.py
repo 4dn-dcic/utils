b6247ff88dd18fd9fd4088010e32d1de7baa8063
William Ronchetti <wrr33@cornell.edu>
2023-11-22 09:02:10 -0500

    build out some remaining functionality

--- dcicutils/transfer_utils.py
+++ dcicutils/transfer_utils.py
@@ -3,7 +3,7 @@ import subprocess
 import concurrent.futures
 from env_utils import is_cgap_env
 from creds_utils import CGAPKeyManager, SMaHTKeyManager
-from ff_utils import search_metadata, get_download_url
+from ff_utils import search_metadata, get_download_url, get_metadata, patch_metadata
 from misc_utils import PRINT
 
 
@@ -23,6 +23,7 @@ class Downloader:
 
 class TransferUtils:
     """ Utility class for downloading files to a local system """
+    O2_PATH_FIELD = 'o2_path'  # set this field on files to indicate download location on o2
 
     def __init__(self, *, ff_env, num_processes=8, download_path, downloader=Downloader.CURL):
         """ Builds the TransferUtils object, initializing Auth etc """
@@ -50,14 +51,42 @@ class TransferUtils:
                 PRINT(f'Could not retrieve download link for {filename} - is it a file type?')
                 mapping[filename] = e
                 continue
+            # this check may need to be revised in case we start sending folks other places
             if '.s3.amazonaws.com' not in download_url:
                 PRINT(f'Potentially bad URL retrieved back from application: {download_url} - continuing')
             mapping[filename] = download_url
         return mapping
 
-    def patch_location_to_portal(self, atid, file_path):
+    def get_exiting_o2_path(self, atid: str) -> Optional[str, None]:
+        """ Does a GET for a file and checks if there is an existing O2 path, if so return the path, else
+            return None
+        """
+        # ensure minimal but up to date view
+        meta = get_metadata(atid, key=self.key, add_on='?datastore=database&frame=raw')
+        if self.O2_PATH_FIELD in meta:
+            return True
+        return False
+
+    def delete_existing_o2_path(self, atid: str) -> dict:
+        """ Deletes an existing download path from a file - do this if it has been removed and you want it
+            tracked or you need to re-trigiger a download """
+        return patch_metadata({}, f'{atid}?delete_fields={self.O2_PATH_FIELD}')
+
+    def patch_location_to_portal(self, atid: str, file_path: str) -> bool:
         """ Patches a special field to atid indicating it is redundantly stored at file_path """
-        pass  # implement me after data model is in
+        path = self.get_exiting_o2_path(atid)
+        if path and file_path != path:
+            PRINT(f'WARNING: patching a new path for file {atid} - ensure file is not present at {path}\n'
+                  f'new path: {file_path}\n'
+                  f'Delete o2_path from existing file in order to proceed')
+            return False
+        elif path and file_path == path:
+            PRINT(f'WARNING: potentially triggering duplicate download of {atid} - double check it has not already '
+                  f'been downloaded to {path} - if it has remove the o2_path before calling again.')
+            return False
+        else:
+            patch_metadata({self.O2_PATH_FIELD: file_path}, atid)
+            return True
 
     @staticmethod
     def download_curl(url: str, filename: str) -> str:
