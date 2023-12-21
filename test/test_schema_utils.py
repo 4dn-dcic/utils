2e122bc4b7914838b8fa224767e0ba157903c68e
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2023-12-21 11:54:20 -0500

    Reformat

--- test/test_schema_utils.py
+++ test/test_schema_utils.py
@@ -8,42 +8,27 @@ REQUIRED = ["bar", "foo"]
 MIXIN_PROPERTIES = [{"$ref": "mixins.json#/link"}]
 ANY_OF_REQUIRED = ["baz", "fu"]
 ANY_OF = [
-        {
-            "type": "string"
-        },
-        {
-            "required": ANY_OF_REQUIRED,
-        }
+    {"type": "string"},
+    {"required": ANY_OF_REQUIRED},
 ]
 ONE_OF_REQUIRED = ["baz", "fa"]
 ONE_OF = [
-        {
-            "foo": "bar"
-        },
-        {
-            "required": ONE_OF_REQUIRED,
-        }
-    ]
+    {"foo": "bar"},
+    {"required": ONE_OF_REQUIRED},
+]
 CONDITIONAL_REQUIRED = ["bar", "baz", "fa", "foo", "fu"]
 IDENTIFYING_PROPERTIES = ["bar", "foo"]
 PROPERTIES = {
-    "foo": {
-        "type": "string",
-    },
+    "foo": {"type": "string"},
     "bar": {
         "type": "object",
         "properties": {
             "baz": {
                 "type": "string",
-            },
+            }
         },
     },
-    "fun": {
-        "type": "array",
-        "items": {
-            "type": "string",
-        },
-    },
+    "fun": {"type": "array", "items": {"type": "string"}},
 }
 SCHEMA = {
     "required": REQUIRED,
@@ -55,35 +40,16 @@ SCHEMA = {
 }
 FORMAT = "email"
 STRING_SCHEMA = {"type": "string", "format": FORMAT}
-ARRAY_SCHEMA = {
-    "type": "array",
-    "items": [STRING_SCHEMA],
-}
-OBJECT_SCHEMA = {
-    "type": "object",
-    "properties": {
-        "foo": STRING_SCHEMA,
-    }
-}
+ARRAY_SCHEMA = {"type": "array", "items": [STRING_SCHEMA]}
+OBJECT_SCHEMA = {"type": "object", "properties": {"foo": STRING_SCHEMA}}
 NUMBER_SCHEMA = {"type": "number"}
 BOOLEAN_SCHEMA = {"type": "boolean"}
 INTEGER_SCHEMA = {"type": "integer"}
 FORMAT_SCHEMA = {
     "type": "string",
     "format": "date-time",
-    "oneOf": [
-        {
-            "format": "date",
-        },
-    ],
-    "anyOf": [
-        {
-            "format": "time",
-        },
-        {
-            "format": "date-time",
-        },
-    ],
+    "oneOf": [{"format": "date"}],
+    "anyOf": [{"format": "time"}, {"format": "date-time"}],
 }
 
 
@@ -92,7 +58,7 @@ FORMAT_SCHEMA = {
     [
         ({}, {}),
         (SCHEMA, PROPERTIES),
-    ]
+    ],
 )
 def test_get_properties(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
     assert schema_utils.get_properties(schema) == expected
@@ -103,7 +69,7 @@ def test_get_properties(schema: Dict[str, Any], expected: Dict[str, Any]) -> Non
     [
         ({}, []),
         (SCHEMA, REQUIRED),
-    ]
+    ],
 )
 def test_get_required(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
     assert schema_utils.get_required(schema) == expected
@@ -114,7 +80,7 @@ def test_get_required(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
     [
         ({}, []),
         (SCHEMA, ANY_OF),
-    ]
+    ],
 )
 def test_get_any_of(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
     assert schema_utils.get_any_of(schema) == expected
@@ -125,7 +91,7 @@ def test_get_any_of(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
     [
         ({}, []),
         (SCHEMA, ONE_OF),
-    ]
+    ],
 )
 def test_get_one_of(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
     assert schema_utils.get_one_of(schema) == expected
@@ -139,7 +105,7 @@ def test_get_one_of(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
         ({"oneOf": ONE_OF}, ONE_OF_REQUIRED),
         ({"required": REQUIRED}, REQUIRED),
         (SCHEMA, CONDITIONAL_REQUIRED),
-    ]
+    ],
 )
 def test_get_conditional_required(
     schema: Dict[str, Any], expected: Dict[str, Any]
@@ -152,11 +118,9 @@ def test_get_conditional_required(
     [
         ({}, []),
         (SCHEMA, ANY_OF_REQUIRED),
-    ]
+    ],
 )
-def test_get_any_of_required(
-    schema: Dict[str, Any], expected: Dict[str, Any]
-) -> None:
+def test_get_any_of_required(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
     assert schema_utils.get_any_of_required(schema) == expected
 
 
@@ -165,11 +129,9 @@ def test_get_any_of_required(
     [
         ({}, []),
         (SCHEMA, ONE_OF_REQUIRED),
-    ]
+    ],
 )
-def test_get_one_of_required(
-    schema: Dict[str, Any], expected: Dict[str, Any]
-) -> None:
+def test_get_one_of_required(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
     assert schema_utils.get_one_of_required(schema) == expected
 
 
@@ -178,11 +140,9 @@ def test_get_one_of_required(
     [
         ({}, []),
         (SCHEMA, MIXIN_PROPERTIES),
-    ]
+    ],
 )
-def test_get_mixin_properties(
-    schema: Dict[str, Any], expected: Dict[str, Any]
-) -> None:
+def test_get_mixin_properties(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
     assert schema_utils.get_mixin_properties(schema) == expected
 
 
@@ -191,7 +151,7 @@ def test_get_mixin_properties(
     [
         ({}, []),
         (SCHEMA, IDENTIFYING_PROPERTIES),
-    ]
+    ],
 )
 def test_get_identifying_properties(
     schema: Dict[str, Any], expected: Dict[str, Any]
@@ -209,11 +169,9 @@ def test_get_identifying_properties(
         (NUMBER_SCHEMA, "number"),
         (BOOLEAN_SCHEMA, "boolean"),
         (INTEGER_SCHEMA, "integer"),
-    ]
+    ],
 )
-def test_get_schema_type(
-    schema: Dict[str, Any], expected: Dict[str, Any]
-) -> None:
+def test_get_schema_type(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
     assert schema_utils.get_schema_type(schema) == expected
 
 
@@ -223,11 +181,9 @@ def test_get_schema_type(
         ({}, False),
         (ARRAY_SCHEMA, False),
         (STRING_SCHEMA, True),
-    ]
+    ],
 )
-def test_is_string_schema(
-    schema: Dict[str, Any], expected: Dict[str, Any]
-) -> None:
+def test_is_string_schema(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
     assert schema_utils.is_string_schema(schema) == expected
 
 
@@ -237,11 +193,9 @@ def test_is_string_schema(
         ({}, False),
         (STRING_SCHEMA, False),
         (ARRAY_SCHEMA, True),
-    ]
+    ],
 )
-def test_is_array_schema(
-    schema: Dict[str, Any], expected: Dict[str, Any]
-) -> None:
+def test_is_array_schema(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
     assert schema_utils.is_array_schema(schema) == expected
 
 
@@ -251,11 +205,9 @@ def test_is_array_schema(
         ({}, False),
         (STRING_SCHEMA, False),
         (OBJECT_SCHEMA, True),
-    ]
+    ],
 )
-def test_is_object_schema(
-    schema: Dict[str, Any], expected: Dict[str, Any]
-) -> None:
+def test_is_object_schema(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
     assert schema_utils.is_object_schema(schema) == expected
 
 
@@ -265,11 +217,9 @@ def test_is_object_schema(
         ({}, False),
         (STRING_SCHEMA, False),
         (NUMBER_SCHEMA, True),
-    ]
+    ],
 )
-def test_is_number_schema(
-    schema: Dict[str, Any], expected: Dict[str, Any]
-) -> None:
+def test_is_number_schema(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
     assert schema_utils.is_number_schema(schema) == expected
 
 
@@ -279,11 +229,9 @@ def test_is_number_schema(
         ({}, False),
         (STRING_SCHEMA, False),
         (BOOLEAN_SCHEMA, True),
-    ]
+    ],
 )
-def test_is_boolean_schema(
-    schema: Dict[str, Any], expected: Dict[str, Any]
-) -> None:
+def test_is_boolean_schema(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
     assert schema_utils.is_boolean_schema(schema) == expected
 
 
@@ -293,11 +241,9 @@ def test_is_boolean_schema(
         ({}, False),
         (STRING_SCHEMA, False),
         (INTEGER_SCHEMA, True),
-    ]
+    ],
 )
-def test_is_integer_schema(
-    schema: Dict[str, Any], expected: Dict[str, Any]
-) -> None:
+def test_is_integer_schema(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
     assert schema_utils.is_integer_schema(schema) == expected
 
 
@@ -306,11 +252,9 @@ def test_is_integer_schema(
     [
         ({}, {}),
         (ARRAY_SCHEMA, [STRING_SCHEMA]),
-    ]
+    ],
 )
-def test_get_items(
-    schema: Dict[str, Any], expected: Dict[str, Any]
-) -> None:
+def test_get_items(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
     assert schema_utils.get_items(schema) == expected
 
 
@@ -320,7 +264,7 @@ def test_get_items(
         ({}, "foo", False),
         (ARRAY_SCHEMA, "foo", False),
         (SCHEMA, "foo", True),
-    ]
+    ],
 )
 def test_has_property(
     schema: Dict[str, Any], property_name: str, expected: Dict[str, Any]
@@ -333,11 +277,9 @@ def test_has_property(
     [
         ({}, ""),
         (STRING_SCHEMA, FORMAT),
-    ]
+    ],
 )
-def test_get_format(
-    schema: Dict[str, Any], expected: Dict[str, Any]
-) -> None:
+def test_get_format(schema: Dict[str, Any], expected: Dict[str, Any]) -> None:
     assert schema_utils.get_format(schema) == expected
 
 
@@ -347,7 +289,7 @@ def test_get_format(
         ({}, []),
         (STRING_SCHEMA, [FORMAT]),
         (FORMAT_SCHEMA, ["date", "date-time", "time"]),
-    ]
+    ],
 )
 def test_get_conditional_formats(
     schema: Dict[str, Any], expected: Dict[str, Any]
