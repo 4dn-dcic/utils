acde8e0e4c9fcfa0c347b9a9efb48d54941af421
David Michaels <david_michaels@hms.harvard.edu>
2024-04-17 11:49:22 -0400

    minor update - test sometimes failing in ga for some reason

--- test/test_progress_bar.py
+++ test/test_progress_bar.py
@@ -1,8 +1,14 @@
 from dcicutils.progress_bar import ProgressBar  # noqa
 
+sleep_seconds = 0
+sleep = lambda: time.sleep(sleep_seconds) if sleep_seconds > 0 else None  # noqa
+
 
 def test_progress_bar_a():
-    pass
+    global sleep
+    total = 1000
+    description = "Working"
+    _ = ProgressBar(total=total, description=description, capture_output_for_testing=True)
 
 
 def test_progress_bar_b():
