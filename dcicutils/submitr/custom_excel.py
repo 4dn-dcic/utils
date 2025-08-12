ac63a96c732cfa9e7cb764c8a7f5764aa5f7692f
sarahgonicholson <sarah_nicholson@hms.harvard.edu>
2025-08-12 10:41:36 -0400

    Fix formatting

--- dcicutils/submitr/custom_excel.py
+++ dcicutils/submitr/custom_excel.py
@@ -115,8 +115,8 @@ class CustomExcel(Excel):
             if not custom_column_mappings:
                 # Fallback to the actual config file in this package.
                 try:
-                    #file = os.path.join(os.path.dirname(__file__), "config", "custom_column_mappings.json")
-                    file = os.path.join("submitr", "config", "custom_column_mappings.json")
+                    # file = os.path.join(os.path.dirname(__file__), "config", "custom_column_mappings.json")
+                    file = os.path.join("submitr", "config", "custom_column_mappings.json") # for testing locally in submitr
                     with io.open(file, "r") as f:
                         custom_column_mappings = json.load(f)
                 except Exception:
